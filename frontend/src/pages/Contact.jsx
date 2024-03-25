0207207216db8c4e88bffd1719acde661f937973
dd-mahn <ducmanh0801@gmail.com>
2024-03-25 16:09:22 +0700

    Initial

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            {/* Your contact page content goes here */}
+        </div>
+    );
+};
+
+export default Contact;
