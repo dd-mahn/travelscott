2c94f7fe9047882a026526a016a69ddbab51db53
dd-mahn <ducmanh0801@gmail.com>
2024-03-25 16:08:21 +0700

    Initial router

--- frontend/src/router/Routers.jsx
+++ frontend/src/router/Routers.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Navigate } from 'react-router-dom';
+import {Routes, Route, Navigate } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Discover from '../pages/Discover';
+import Contact from '../pages/Contact';
 
 const Routers = () => {
     return (
-        <Router>
-        </Router>
+        <Routes>
+             <Route path='/' element={<Navigate to='/home'/>} />
+             <Route path='/home' element={<Home/>}/>
+             <Route path='/about' element={<About/>}/>
+             <Route path='/discover' element={<Discover/>}/>
+             <Route path='/contact' element={<Contact/>}/>
+        </Routes>
     );
 };
 
