e64d983a319d6560eab102b74347738d10de9e5a
dd-mahn <ducmanh0801@gmail.com>
2024-04-26 23:28:23 +0700

    Initial

---
+++ frontend/src/pages/UnderConstruction.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function UnderConstruction() {
+  return (
+    <Spline scene="https://prod.spline.design/AdcHA0Pi9dvDom4D/scene.splinecode" />
+  );
+}
