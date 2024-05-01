18a9bfb015fbee93a893d1c5c729ed654fa973cc
dd-mahn <ducmanh0801@gmail.com>
2024-05-02 02:51:58 +0700

    Update destination routes to include new endpoints and import statements

--- backend/routes/destination.js
+++ backend/routes/destination.js
@@ -1,17 +1,32 @@
-import { createDestination, getDestinations, getSingleDestination, updateDestination, deleteDestination, updateDestinationPlaces, updateDestinationReviews, updateDestinationTransportation, deleteAllDestinations, updateDestinationImages } from "../controllers/DestinationControllers.js";
-import express from "express"
+import {
+  createDestination,
+  getDestinations,
+  getSingleDestination,
+  updateDestination,
+  deleteDestination,
+  updateDestinationPlaces,
+  updateDestinationReviews,
+  updateDestinationTransportation,
+  deleteAllDestinations,
+  updateDestinationImages,
+  getCountries,
+  getDestinationCount
+} from "../controllers/DestinationControllers.js";
+import express from "express";
 
 const router = express.Router();
 
-router.post('/', createDestination);
-router.get('/', getDestinations);
-router.get('/:id', getSingleDestination);
-router.put('/:id', updateDestination);
-router.delete('/:id', deleteDestination);
-router.delete('/', deleteAllDestinations)
-router.put('/:id/places', updateDestinationPlaces);
-router.put('/:id/transportation', updateDestinationTransportation);
-router.put('/:id/reviews', updateDestinationReviews);
-router.put('/:id/images', updateDestinationImages);
+router.post("/", createDestination);
+router.get("/", getDestinations);
+router.get("/count", getDestinationCount);
+router.get("/countries", getCountries);
+router.get("/:id", getSingleDestination);
+router.put("/:id", updateDestination);
+router.delete("/:id", deleteDestination);
+router.delete("/", deleteAllDestinations);
+router.put("/:id/places", updateDestinationPlaces);
+router.put("/:id/transportation", updateDestinationTransportation);
+router.put("/:id/reviews", updateDestinationReviews);
+router.put("/:id/images", updateDestinationImages);
 
 export default router;
