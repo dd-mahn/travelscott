87bf2f22c0a735cab79a1f70145805db89c6a62e
dd-mahn <ducmanh0801@gmail.com>
2024-05-01 22:42:54 +0700

    Update Gallery component to remove commented code and import styles

--- frontend/src/pages/HomeComponents/Gallery.jsx
+++ frontend/src/pages/HomeComponents/Gallery.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { Carousel } from 'react-responsive-carousel';
-// import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../../styles/home.css';
 import MarqueeSlider from 'react-marquee-slider';
 import times from 'lodash.times';
