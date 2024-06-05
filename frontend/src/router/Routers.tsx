a80c6c01ee9b18619b9e647d0933440c10381423
dd-mahn <ducmanh0801@gmail.com>
2024-06-06 03:14:57 +0700

    Update router paths and components for new design

--- frontend/src/router/Routers.tsx
+++ frontend/src/router/Routers.tsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Discover from "../pages/Discover";
-import Contact from "../pages/Contact";
-import Test from "../pages/Test";
-import Destination from "../pages/Destination";
+import { Routes, Route } from "react-router-dom";
+
+// Old Design Components
+import OldHome from "src/pages/OldDesign/components/Home";
+import OldAbout from "src/pages/OldDesign/components/About";
+import OldDiscover from "src/pages/OldDesign/components/Discover";
+import OldDestination from "src/pages/OldDesign/components/Destination";
+
+// New Design Components
+import Contact from "src/pages/Contact";
+import Test from "src/pages/Test";
+import Home from "src/pages/Home";
+
 
 const Routers = () => {
   return (
     <Routes>
+      {/* Old routes */}
+      {/* <Route path="/old/" element={<OldHome />} />
+      <Route path="/old/about" element={<OldAbout />} />
+      <Route path="/old/destinations" element={<OldDiscover />} />
+      <Route path="/old/destinations/:id" element={<OldDestination />} /> */}
+
+      {/* New routes */}
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/destinations" element={<Discover />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/test" element={<Test />} />
-      <Route path="/destinations/:id" element={<Destination />} />
       
     </Routes>
   );
