bae3c9e20a8303b0f5fc18500aaffc2763f6c4fe
dd-mahn <ducmanh0801@gmail.com>
2024-07-04 15:29:45 +0700

    Update new footer layout and styles

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "src/components/Footer/footer.css";
 import { NavLink } from "react-router-dom";
+import planeIcon from "src/assets/svg/plane-icon.svg";
+import footerVideo from "src/assets/videos/footer.mp4";
 
 const sitemap = [
   {
@@ -16,8 +18,8 @@ const sitemap = [
     display: "Discover",
   },
   {
-    path: "/insight",
-    display: "Insight",
+    path: "/inspo",
+    display: "Inspiration",
   },
   {
     path: "/contact",
@@ -44,29 +46,43 @@ const socials = [
   },
 ];
 
+const otherLinks = [
+  {
+    path: "/privacy",
+    display: "Privacy Policy",
+  },
+];
+
 const Footer = () => {
   return (
-    <footer className="px-sect flex flex-col self-end border-t-2 border-solid border-text-light pb-8 lg:pt-20 xl:pt-20 2xl:pt-sect-default 3xl:pt-sect-default">
-      <div className="flex justify-center">
-        <h1 className="font-kaushan text-main-green">TravelScott</h1>
+    <footer className="flex flex-col self-end border-t border-solid border-gray xl:pt-20 2xl:pt-20">
+      <div className="px-sect flex items-end justify-between">
+        <p>
+          Made and curated by people with <br />
+          passion in travel, Travel, and TRAVEL.
+        </p>
+        <div className="border-b border-solid border-text-light">
+          <label htmlFor="subscribe-email" className="uppercase">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="subscribe-email"
+            className="border-0 bg-transparent focus:outline-none"
+          />
+          <button title="subscribe">
+            {" "}
+            <img src={planeIcon} alt="" />{" "}
+          </button>
+        </div>
       </div>
 
-      <div className="flex justify-between">
-        <div className="flex flex-col justify-end lg:gap-2 xl:gap-2 2xl:gap-3 3xl:gap-4">
-          <button
-            className="underline__btn"
-            onClick={() => window.scrollTo(0, 0)}
-          >
-            Back to top <i className="ri-arrow-up-line"></i>
-          </button>
-          <p>
-            An application made by people <br></br> with travel and creative
-            passion.
-          </p>
-        </div>
-
+      <div className="px-sect flex items-end justify-between">
         <div className="flex lg:gap-8 xl:gap-10 2xl:gap-12 3xl:gap-12">
-          <nav>
+          <nav className="flex flex-col lg:gap-2 2xl:gap-4">
+            <span className="font-medium text-text-light lg:text-lg 2xl:text-xl">
+              Sitemap
+            </span>
             <ul className="flex flex-col justify-start">
               {sitemap.map((item, index) => (
                 <li key={index}>
@@ -76,7 +92,10 @@ const Footer = () => {
             </ul>
           </nav>
 
-          <nav>
+          <nav className="flex flex-col lg:gap-2 2xl:gap-4">
+            <span className="font-medium text-text-light lg:text-lg 2xl:text-xl">
+              Socials
+            </span>
             <ul className="flex flex-col justify-start">
               {socials.map((item, index) => (
                 <li key={index}>
@@ -86,6 +105,36 @@ const Footer = () => {
             </ul>
           </nav>
         </div>
+
+        <button className="underline-btn" onClick={() => window.scrollTo(0, 0)}>
+          Back to top <i className="ri-arrow-up-line"></i>
+        </button>
+      </div>
+
+      <div className="flex justify-center border-t border-solid border-gray">
+        {/* <video
+          autoPlay
+          muted
+          loop
+          src={footerVideo}
+          className="absolute left-0 top-0 h-full w-full object-cover"
+        ></video> */}
+        <h1 className="font-kaushan select-none text-text-light lg:mr-16 2xl:mr-32 lg:text-8xl xl:text-13xl 2xl:text-14xl 3xl:text-15xl">TravelScott</h1>
+      </div>
+
+      <div className="px-sect flex flex-row items-center justify-between border-t border-solid border-gray lg:h-8 2xl:h-10">
+        <span className="select-none font-medium uppercase text-gray lg:text-base xl:text-sm 2xl:text-lg">
+          Copyright TravelScott 2024
+        </span>
+        <nav>
+          <ul className="flex flex-row justify-start">
+            {otherLinks.map((item, index) => (
+              <li key={index}>
+                <NavLink to={item.path}>{item.display}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </footer>
   );
