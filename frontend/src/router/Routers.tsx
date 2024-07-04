72a73c0ae2d8b19f84e30e17625a0ff2a35ab37c
dd-mahn <ducmanh0801@gmail.com>
2024-07-05 01:20:36 +0700

    Update router configuration and component imports

--- frontend/src/router/Routers.tsx
+++ frontend/src/router/Routers.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-// Old Design Components
-import OldHome from "src/pages/OldDesign/components/Home";
-import OldAbout from "src/pages/OldDesign/components/About";
-import OldDiscover from "src/pages/OldDesign/components/Discover";
-import OldDestination from "src/pages/OldDesign/components/Destination";
-
-// New Design Components
+import About from "src/pages/About";
 import Contact from "src/pages/Contact";
 import Test from "src/pages/Test";
 import Home from "src/pages/Home";
@@ -16,13 +10,7 @@ import Home from "src/pages/Home";
 const Routers = () => {
   return (
     <Routes>
-      {/* Old routes */}
-      {/* <Route path="/old/" element={<OldHome />} />
-      <Route path="/old/about" element={<OldAbout />} />
-      <Route path="/old/destinations" element={<OldDiscover />} />
-      <Route path="/old/destinations/:id" element={<OldDestination />} /> */}
-
-      {/* New routes */}
+      <Route path="/about" element={<About />} />
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/test" element={<Test />} />
