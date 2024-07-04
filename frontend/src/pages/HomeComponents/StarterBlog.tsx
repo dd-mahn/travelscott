c17c76ea14a9858e2de0e09900cc2329cf2040d9
dd-mahn <ducmanh0801@gmail.com>
2024-07-04 15:30:59 +0700

    Add comments for readability and maintainability

--- frontend/src/pages/HomeComponents/StarterBlog.tsx
+++ frontend/src/pages/HomeComponents/StarterBlog.tsx
@@ -1,6 +1,13 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import Blog from "src/types/Blog";
+import React, {
+  useCallback,
+  useEffect,
+  useReducer,
+  useMemo,
+  useState,
+} from "react";
+import BlogType from "src/types/Blog";
 
+// Interfaces and types
 interface Position {
   x: number;
   y: number;
@@ -12,67 +19,156 @@ interface Positions {
 }
 
 interface StarterBlogsProps {
-  blogs: Blog[];
+  blogs: BlogType[];
 }
 
+type Action =
+  | { type: "SET_INITIAL_POSITIONS"; payload: Positions }
+  | { type: "UPDATE_POSITION"; payload: { title: string; position: Position } }
+  | { type: "INCREMENT_Z_INDEX"; payload: { title: string; zIndex: number } };
+
+// Reducer function
+
+const positionReducer = (state: Positions, action: Action): Positions => {
+  switch (action.type) {
+    case "SET_INITIAL_POSITIONS":
+      return action.payload;
+    case "UPDATE_POSITION":
+      const { title, position } = action.payload;
+      return { ...state, [title]: position };
+    case "INCREMENT_Z_INDEX":
+      return {
+        ...state,
+        [action.payload.title]: {
+          ...state[action.payload.title],
+          zIndex: action.payload.zIndex,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+// Component
+const BlogComponent = ({
+  blog,
+  onDragStart,
+  position,
+}: {
+  blog: BlogType;
+  onDragStart: (event: React.MouseEvent<HTMLDivElement>) => void;
+  position: Position;
+}) => (
+  <div
+    key={blog.title}
+    className="blog absolute flex h-0.5svh w-1/3 flex-col justify-between gap-4 overflow-hidden rounded-xl bg-background-light pb-8 shadow-xl"
+    style={{
+      left: `${position.x}px`,
+      top: `${position.y}px`,
+      zIndex: position.zIndex,
+    }}
+    onMouseDown={onDragStart}
+  >
+    <div
+      className="flex h-3/4 flex-col items-start justify-end gap-0 px-8 pb-4"
+      style={{
+        backgroundImage: `url(${blog.image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <span className="font-medium text-text-dark lg:text-sm 2xl:text-base">
+        {blog.author}
+      </span>
+      <h2 className="font-semibold uppercase text-text-dark lg:text-lg xl:text-xl 2xl:text-2xl">
+        {blog.title}
+      </h2>
+    </div>
+    <div className="flex flex-col gap-4 px-8">
+      <p className="w-full overflow-hidden font-normal lg:text-base xl:text-lg 2xl:text-xl">
+        {blog.content[0].sectionText[0]}
+      </p>
+      <button className="underline-btn uppercase">
+        View<i className="ri-arrow-right-up-line"></i>
+      </button>
+    </div>
+  </div>
+);
+
+// Main component
 const StarterBlogs: React.FC<StarterBlogsProps> = ({ blogs }) => {
-  const [positions, setPositions] = useState<Positions>({});
+
+  // Handling move and drag behavior 
+  const [positions, dispatch] = useReducer(positionReducer, {});
   const [maxZIndex, setMaxZIndex] = useState(1);
 
   useEffect(() => {
-    const screenCenterX = window.innerWidth / 2 - 600;
-    const screenCenterY = window.innerHeight / 2;
     const initialPositions: Positions = {};
-
     blogs.forEach((blog) => {
-      const x = Math.random() * 500 + screenCenterX - 100;
-      const y = Math.random() * 200 + screenCenterY - 100;
-      initialPositions[blog.title] = { x, y, zIndex: 1 };
+      initialPositions[blog.title] = {
+        x: Math.random() * 500 + window.innerWidth / 2 - 600 - 100,
+        y: Math.random() * 200 + window.innerHeight / 2 - 100,
+        zIndex: 1,
+      };
     });
-
-    setPositions(initialPositions);
+    dispatch({ type: "SET_INITIAL_POSITIONS", payload: initialPositions });
   }, [blogs]);
+
+  const updatePosition = useCallback(
+    (title: string, x: number, y: number, zIndex: number) => {
+      dispatch({
+        type: "UPDATE_POSITION",
+        payload: {
+          title,
+          position: { x, y, zIndex },
+        },
+      });
+    },
+    [],
+  );
+
+  const incrementZIndex = useCallback(
+    (title: string) => {
+      const newPosZIndex = maxZIndex + 1;
+      setMaxZIndex(newPosZIndex);
+      dispatch({
+        type: "INCREMENT_Z_INDEX",
+        payload: { title, zIndex: newPosZIndex },
+      });
+    },
+    [maxZIndex],
+  );
 
   const handleDragStart = useCallback(
     (event: React.MouseEvent<HTMLDivElement>, title: string) => {
-      const newPosZIndex = maxZIndex + 1;
-      const startPos = positions[title] || { x: 0, y: 0, zIndex: newPosZIndex };
-
-      setPositions((prev) => ({
-        ...prev,
-        [title]: { ...startPos, zIndex: newPosZIndex },
-      }));
-      setMaxZIndex(newPosZIndex);
+      incrementZIndex(title);
 
       const startX = event.pageX;
       const startY = event.pageY;
+      const initialX = positions[title].x;
+      const initialY = positions[title].y;
 
-      const handleMouseMove = (moveEvent: MouseEvent) => {
-        const newX = startPos.x + (moveEvent.pageX - startX);
-        const newY = startPos.y + (moveEvent.pageY - startY);
-
-        setPositions((prev) => ({
-          ...prev,
-          [title]: { x: newX, y: newY, zIndex: newPosZIndex },
-        }));
+      const moveHandler = (moveEvent: MouseEvent) => {
+        const newPos = {
+          x: initialX + (moveEvent.pageX - startX),
+          y: initialY + (moveEvent.pageY - startY),
+          zIndex: maxZIndex + 1,
+        };
+        updatePosition(title, newPos.x, newPos.y, newPos.zIndex);
       };
 
-      const handleMouseUp = () => {
-        document.removeEventListener("mousemove", handleMouseMove);
-        document.removeEventListener("mouseup", handleMouseUp);
+      const upHandler = () => {
+        document.removeEventListener("mousemove", moveHandler);
+        document.removeEventListener("mouseup", upHandler);
       };
 
-      document.addEventListener("mousemove", handleMouseMove);
-      document.addEventListener("mouseup", handleMouseUp);
-
-      return () => {
-        document.removeEventListener("mousemove", handleMouseMove);
-        document.removeEventListener("mouseup", handleMouseUp);
-      };
+      document.addEventListener("mousemove", moveHandler);
+      document.addEventListener("mouseup", upHandler);
     },
-    [maxZIndex, positions],
+    [incrementZIndex, maxZIndex, positions, updatePosition],
   );
 
+  // Displaying starter blogs
   const starterBlogs = useMemo(
     () => blogs.filter((blog) => blog.category === "Starter" && blog.image),
     [blogs],
@@ -81,40 +177,14 @@ const StarterBlogs: React.FC<StarterBlogsProps> = ({ blogs }) => {
   return (
     <>
       {starterBlogs.map((blog) => (
-        <div
-          key={blog.title} 
-          className="blog absolute flex h-0.5svh w-1/3 flex-col justify-between gap-4 overflow-hidden rounded-xl bg-background-light pb-8 shadow-xl"
-          style={{
-            left: `${positions[blog.title]?.x}px`,
-            top: `${positions[blog.title]?.y}px`,
-            zIndex: positions[blog.title]?.zIndex,
-          }}
-          onMouseDown={(event) => handleDragStart(event, blog.title)}
-        >
-          <div
-            className="flex h-3/4 flex-col items-start justify-end gap-0 px-8 pb-4"
-            style={{
-              backgroundImage: `url(${blog.image})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            <span className="font-medium text-text-dark lg:text-sm 2xl:text-base">
-              {blog.author}
-            </span>
-            <h2 className="font-semibold uppercase text-text-dark lg:text-lg xl:text-xl 2xl:text-2xl">
-              {blog.title}
-            </h2>
-          </div>
-          <div className="flex flex-col gap-4 px-8">
-            <p className="w-full overflow-hidden font-medium lg:text-base xl:text-lg 2xl:text-xl">
-              {blog.content[0].sectionText[0]}
-            </p>
-            <button className="underline-btn uppercase">
-              View<i className="ri-arrow-right-up-line"></i>
-            </button>
-          </div>
-        </div>
+        <BlogComponent
+          key={blog.title}
+          blog={blog}
+          onDragStart={(event: React.MouseEvent<HTMLDivElement>) =>
+            handleDragStart(event, blog.title)
+          }
+          position={positions[blog.title] || { x: 0, y: 0, zIndex: 1 }}
+        />
       ))}
     </>
   );
