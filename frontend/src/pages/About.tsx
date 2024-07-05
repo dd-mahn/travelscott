a03ef3754a98e86bbd244e688105e58d6f0a4765
dd-mahn <ducmanh0801@gmail.com>
2024-07-05 16:28:43 +0700

    finish about layout and basic styles

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -11,6 +11,9 @@ import who5 from "src/assets/images/ui/about/about-5.jpg";
 import who6 from "src/assets/images/ui/about/about-6.jpg";
 import who7 from "src/assets/images/ui/about/about-7.jpg";
 import who8 from "src/assets/images/ui/about/about-8.jpg";
+import airplane1 from "src/assets/svg/airplane-1.svg";
+import airplane2 from "src/assets/svg/airplane-2.svg";
+import airplane3 from "src/assets/svg/airplane-3.svg";
 
 const people = [
   {
@@ -61,20 +64,20 @@ const About: React.FC = () => {
       {/* HERO SECTION */}
 
       <section className="hero px-sect flex flex-col items-center gap-12 py-sect-short">
-        <h1 className="pt-sect-short text-center font-prima font-bold uppercase text-main-green lg:text-8xl 2xl:text-10xl">
+        <h1 className="pt-sect-short text-center h1-md text-main-green">
           Travel
-          <span className="font-kaushan font-medium normal-case text-text-light">
+          <span className="font-logo font-medium normal-case text-text-light">
             Scott,
           </span>{" "}
           <span className="text-text-light">your</span> guide
         </h1>
 
-        <p className="text-center font-medium lg:w-2/5 lg:text-lg xl:w-2/5 xl:text-xl 2xl:w-1/3 2xl:text-1.5xl 3xl:w-1/3 3xl:text-2xl">
+        <p className="text-center p-medium lg:w-2/5 xl:w-2/5 2xl:w-1/3 3xl:w-1/3">
           We simply want to awaken the passion for <br />
           travel within you.
         </p>
 
-        <p className="text-center font-medium lg:w-2/5 lg:text-lg xl:w-2/5 xl:text-xl 2xl:w-1/3 2xl:text-1.5xl 3xl:w-1/3 3xl:text-2xl">
+        <p className="text-center p-medium lg:w-2/5 xl:w-2/5 2xl:w-1/3 3xl:w-1/3">
           We simplify your travel experience.
         </p>
 
@@ -90,22 +93,22 @@ const About: React.FC = () => {
       {/* STACKED SECTION */}
       <section className="flex flex-col items-center justify-start py-sect-semi">
         {/* HOW SECTION */}
-        <h2 className="text-center font-medium leading-normal tracking-tight lg:text-3.5xl xl:text-4xl 2xl:text-4.5xl 3xl:text-5xl">
+        <h2 className="text-center h3-inter">
           How?
         </h2>
         <div className="how relative">
-          <div className="sticky top-48 z-0 mx-auto mt-sect-medium flex h-0.5svh w-3/4 flex-row items-start justify-between rounded-xl bg-background-light px-8 pb-sect-short pt-4 shadow-section">
+          <div className="sticky lg:top-24 2xl:top-48 z-0 mx-auto mt-sect-medium mb-24 flex h-0.5svh lg:w-2/3 2xl:w-3/4 flex-row items-start justify-between rounded-xl bg-background-light px-8 pb-sect-short pt-4 shadow-section">
             <div className="flex flex-col items-start justify-start gap-12">
               <div className="flex flex-col justify-start gap-0">
-                <h1 className="font-prima font-bold uppercase leading-snug lg:text-8xl 2xl:text-10xl">
+                <h1 className="h1-md-bold">
                   Optimal
                 </h1>
-                <h2 className="font-sans font-semibold uppercase lg:text-xl 2xl:text-1.5xl">
+                <span className="span-medium uppercase">
                   Information
-                </h2>
+                </span>
               </div>
 
-              <p className="font-medium lg:w-3/4 lg:text-base xl:w-3/4 xl:text-lg 2xl:w-2/5 2xl:text-xl 3xl:w-2/3 3xl:text-xl">
+              <p className="p-medium lg:w-3/4 xl:w-3/4 2xl:w-2/5 3xl:w-2/3">
                 From the must-see landmarks to the hidden gems, our guides are
                 designed to ensure you’re well-informed. Whether it’s an
                 upcoming journey or a place you’re curious about, our resources
@@ -114,27 +117,27 @@ const About: React.FC = () => {
               </p>
             </div>
 
-            <h1 className="text-stroke stroke-text-light font-prima font-bold uppercase text-transparent lg:text-8xl 2xl:text-10xl">
+            <h1 className="text-stroke h1-md-bold text-transparent">
               01
             </h1>
           </div>
 
-          <div className="sticky top-72 z-0 mx-auto mt-sect-medium flex h-0.5svh w-3/4 flex-row items-start justify-between rounded-xl bg-light-brown px-8 pb-sect-short pt-4 shadow-section">
-            <h1 className="text-stroke font-prima font-bold uppercase text-transparent lg:text-8xl 2xl:text-10xl">
+          <div className="sticky lg:top-48 2xl:top-72 z-0 mx-auto mt-sect-medium mb-24 flex h-0.5svh lg:w-2/3 2xl:w-3/4 flex-row items-start justify-between rounded-xl bg-light-brown px-8 pb-sect-short pt-4 shadow-section">
+            <h1 className="text-stroke h1-md-bold text-transparent">
               02
             </h1>
 
             <div className="flex flex-col items-end justify-start gap-12">
               <div className="flex flex-col justify-end gap-0">
-                <h1 className="font-prima font-bold uppercase leading-snug lg:text-8xl 2xl:text-10xl">
+                <h1 className="h1-md-bold">
                   Vibrant
                 </h1>
-                <h2 className="text-end font-sans font-semibold uppercase lg:text-xl 2xl:text-1.5xl">
+                <h2 className="text-end font-sans span-medium uppercase">
                   Experience
                 </h2>
               </div>
 
-              <p className="font-medium lg:w-3/4 lg:text-base xl:w-3/4 xl:text-lg 2xl:w-2/5 2xl:text-xl 3xl:w-2/3 3xl:text-xl">
+              <p className="p-medium lg:w-3/4 xl:w-3/4 2xl:w-2/5 3xl:w-2/3">
                 We provide a streamlined research experience with high-quality
                 visual content that aims to inspire your travel plans. Each
                 search is an opportunity for discovery, and our vivid imagery
@@ -145,18 +148,18 @@ const About: React.FC = () => {
             </div>
           </div>
 
-          <div className="sticky top-96 z-0 mx-auto mt-sect-medium flex h-0.5svh w-3/4 flex-row items-start justify-between rounded-xl bg-light-green px-8 pb-sect-short pt-4 shadow-section">
+          <div className="sticky lg:top-72 2xl:top-96 z-0 mx-auto mt-sect-medium mb-24 flex h-0.5svh lg:w-2/3 2xl:w-3/4 flex-row items-start justify-between rounded-xl bg-light-green px-8 pb-sect-short pt-4 shadow-section">
             <div className="flex flex-col items-start justify-start gap-12">
               <div className="flex flex-col justify-start gap-0">
-                <h1 className="font-prima font-bold uppercase leading-snug lg:text-8xl 2xl:text-10xl">
+                <h1 className="h1-md-bold">
                   Verified
                 </h1>
-                <h2 className="font-sans font-semibold uppercase lg:text-xl 2xl:text-1.5xl">
+                <span className="font-sans span-medium uppercase ">
                   Resources
-                </h2>
+                </span>
               </div>
 
-              <p className="font-medium lg:w-3/4 lg:text-base xl:w-3/4 xl:text-lg 2xl:w-2/5 2xl:text-xl 3xl:w-2/3 3xl:text-xl">
+              <p className="p-medium lg:w-3/4 xl:w-3/4 2xl:w-2/5 3xl:w-2/3">
                 Our platform curates content from renowned travel
                 websites and authentic traveler reviews to create a
                 comprehensive travel resource. Rest assured, the credibility of
@@ -165,7 +168,7 @@ const About: React.FC = () => {
               </p>
             </div>
 
-            <h1 className="text-stroke font-prima font-bold uppercase text-transparent lg:text-8xl 2xl:text-10xl">
+            <h1 className="text-stroke h1-md-bold text-transparent">
               03
             </h1>
           </div>
@@ -173,11 +176,11 @@ const About: React.FC = () => {
           <div className="py-sect-default"></div>
 
           {/* WHO SECTION */}
-          <section className="who px-sect sticky top-0 z-10 rounded-5xl bg-background-dark py-sect-default shadow-section">
+          <section className="who px-sect sticky top-0 z-10 rounded-5xl bg-background-dark lg:py-sect-default shadow-section">
             <div className="relative flex flex-col gap-sect-default">
               <div className="blob-1 blur-blob z-0 h-1/4 w-1/4"></div>
               <div className="blob-2 blur-blob z-0 h-3/5 w-3/5"></div>
-              <h2 className="text-center font-medium leading-normal tracking-tight text-text-dark lg:text-3.5xl xl:text-4xl 2xl:text-4.5xl 3xl:text-5xl">
+              <h2 className="text-center text-text-dark h3-inter">
                 Who?
               </h2>
               <div className="z-10 flex flex-wrap justify-center gap-12">
@@ -195,10 +198,10 @@ const About: React.FC = () => {
                       }}
                     ></div>
                     <div className="flex flex-col gap-2">
-                      <h3 className="text-center font-medium text-text-dark lg:text-lg xl:text-xl 2xl:text-1.5xl">
+                      <h3 className="text-center span-regular text-text-dark">
                         {person.name}
                       </h3>
-                      <p className="text-center font-normal text-text-dark lg:text-base xl:text-base 2xl:text-lg">
+                      <p className="text-center span-small text-text-dark">
                         {person.role}
                       </p>
                     </div>
@@ -207,15 +210,15 @@ const About: React.FC = () => {
               </div>
               <div className="px-sect relative pb-sect-default pt-sect-long">
                 <div className="blob-3 blur-blob z-0 h-3/5 w-3/5"></div>
-                <img src={who1} alt="" className="img-1 absolute" />
-                <img src={who2} alt="" className="img-2 absolute" />
-                <img src={who3} alt="" className="img-3 absolute" />
-                <img src={who4} alt="" className="img-4 absolute" />
-                <img src={who5} alt="" className="img-5 absolute" />
-                <img src={who6} alt="" className="img-6 absolute" />
-                <img src={who7} alt="" className="img-7 absolute" />
-                <img src={who8} alt="" className="img-8 absolute" />
-                <p className="font-normal text-text-dark lg:w-2/3 lg:text-base xl:w-2/3 xl:text-lg 2xl:w-2/5 2xl:text-xl 3xl:text-xl">
+                <img src={who1} alt="" className="img-1 absolute rounded-lg" />
+                <img src={who2} alt="" className="img-2 absolute rounded-lg" />
+                <img src={who3} alt="" className="img-3 absolute rounded-lg" />
+                <img src={who4} alt="" className="img-4 absolute rounded-lg" />
+                <img src={who5} alt="" className="img-5 absolute rounded-lg" />
+                <img src={who6} alt="" className="img-6 absolute rounded-lg" />
+                <img src={who7} alt="" className="img-7 absolute rounded-lg" />
+                <img src={who8} alt="" className="img-8 absolute rounded-lg" />
+                <p className="p-regular text-text-dark lg:w-2/3 xl:w-2/3 2xl:w-1/2 3xl:w-2/5">
                   In 2024, we came together, fueled by an unwavering passion for
                   adventure and a deep-seated commitment to unveiling the
                   splendor of our planet. Our aspiration is to witness the joy
@@ -230,13 +233,16 @@ const About: React.FC = () => {
       </section>
 
       {/* WHY SECTION */}
-      <section className="why px-sect flex flex-col items-center pt-sect-default pb-sect-semi gap-48">
-        <h2 className="text-center font-medium leading-normal tracking-tight lg:text-3.5xl xl:text-4xl 2xl:text-4.5xl 3xl:text-5xl">
+      <section className="why px-sect relative flex flex-col items-center pb-sect-semi pt-sect-default lg:gap-48 2xl:gap-64">
+        <img src={airplane1} alt="" className="plane-1 absolute" />
+        <img src={airplane2} alt="" className="plane-2 absolute" />
+        <img src={airplane3} alt="" className="plane-3 absolute" />
+        <h2 className="text-center h3-inter">
           Why?
         </h2>
-        <p className="font-medium text-text-light lg:text-base xl:text-lg 2xl:text-1.5xl 3xl:text-1.5xl">
-          “A paper plane’s journey, however brief, reminds us that <br /> even the
-          simplest dreams can take flight.”
+        <p className="p-large">
+          “A paper plane’s journey, however brief, reminds us that <br /> even
+          the simplest dreams can take flight.”
         </p>
       </section>
     </main>
