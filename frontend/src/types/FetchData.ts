7dd544fedec8280e7cf1aa0d702bedb45d04b3e5
dd-mahn <ducmanh0801@gmail.com>
2024-07-12 01:01:15 +0700

    Refactor FetchData types and add FetchBlogsType and FetchCountriesType

--- frontend/src/types/FetchData.ts
+++ frontend/src/types/FetchData.ts
@@ -1,11 +1,14 @@
-import {Destination} from "src/types/Destination";
+import Destination from "src/types/Destination";
+import Country from "./Country";
+import Blog from "./Blog";
 
 export interface FetchData<T> {
     result: T[];
-    totalCount?: number;
+    count?: number;
     page?: string;
     totalPages?: number;
   }
   
 export type FetchDestinationType = FetchData<Destination>;
-export type FetchCountriesType = FetchData<string>;
+export type FetchCountriesType = FetchData<Country>;
+export type FetchBlogsType = FetchData<Blog>;
