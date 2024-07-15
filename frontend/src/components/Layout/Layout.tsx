3022e841c00a556dfdf053b3c279a7136d0c208d
dd-mahn <ducmanh0801@gmail.com>
2024-07-15 16:30:30 +0700

    feat: Add scroll to top functionality on route change in Layout component

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "src/components/Header/Header";
 import Footer from "src/components/Footer/Footer";
 import Routers from "src/router/Routers";
+import { useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Header />
