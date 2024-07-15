00d44474b52e75c2d7ae119310af5e144c0f2fb4
dd-mahn <ducmanh0801@gmail.com>
2024-07-15 16:03:43 +0700

    Finish discover page (missing blogs) basic structure and styles

--- frontend/src/pages/Discover.tsx
+++ frontend/src/pages/Discover.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import useFetch from "src/hooks/useFetch";
 import Country from "src/types/Country";
 import Destination from "src/types/Destination";
 import { BASE_URL } from "src/utils/config";
+import { useNavigate } from "react-router-dom";
 
 import "src/styles/discover.css";
 
@@ -12,36 +13,25 @@ import africaMap from "src/assets/images/ui/maps/Africa.png";
 import northAmericaMap from "src/assets/images/ui/maps/NorthAmerica.png";
 import southAmericaMap from "src/assets/images/ui/maps/SouthAmerica.png";
 import oceaniaMap from "src/assets/images/ui/maps/Oceania.png";
-
-const getFeaturedDestinations = (destinations: Destination[]) => {
-  if (destinations !== undefined) {
-    let featuredDestinations = destinations.filter(
-      (destination) => destination.featured === true,
-    );
-    if (featuredDestinations.length > 5) {
-      featuredDestinations = featuredDestinations.slice(0, 5);
-    }
-    return featuredDestinations;
-  } else {
-    return [];
-  }
-};
-
-const getCountryByContinent = (countries: Country[], continent: string) => {
-  if (countries !== undefined) {
-    return countries.filter((country) => country.continent === continent);
-  } else {
-    return [];
-  }
-};
+import { getFeaturedDestinations } from "src/utils/getFeaturedDestinations";
+import { getCountryByContinent } from "src/utils/getCountryByContinent";
+import Pagination from "src/components/ui/Pagination";
+import DestinationCard from "src/components/ui/DestinationCard";
+import CountryCard from "src/components/ui/CountryCard";
 
 const Discover: React.FC = () => {
-  // Fetch data
+  const [currentPage, setCurrentPage] = useState(1);
+  const [destinations, setDestinations] = useState<Destination[]>([]);
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [selectedContinent, setSelectedContinent] = useState("Asia");
+
+  // Fetch data from API
   const {
     data: destinationData,
     loading: destinationLoading,
     error: destinationError,
-  } = useFetch(`${BASE_URL}/destinations`);
+  } = useFetch(`${BASE_URL}/destinations?page=${currentPage}`, [currentPage]);
+
   const {
     data: countryData,
     loading: countryLoading,
@@ -49,56 +39,68 @@ const Discover: React.FC = () => {
   } = useFetch(`${BASE_URL}/countries`);
 
   // Handle fetched data for rendering
-  const countries = countryData?.result;
-  const destinations = destinationData?.result;
+  const totalDestinations = destinationData?.count as number;
+  useEffect(() => {
+    if (destinationData?.result) {
+      setDestinations(destinationData?.result as Destination[]);
+    }
 
-  const continents = [
-    {
-      name: "Asia",
-      countries: getCountryByContinent(countries as Country[], "Asia"),
-      count: getCountryByContinent(countries as Country[], "Asia").length,
-      image: asiaMap,
-    },
-    {
-      name: "Africa",
-      countries: getCountryByContinent(countries as Country[], "Africa"),
-      count: getCountryByContinent(countries as Country[], "Africa").length,
-      image: africaMap,
-    },
-    {
-      name: "Europe",
-      countries: getCountryByContinent(countries as Country[], "Europe"),
-      count: getCountryByContinent(countries as Country[], "Europe").length,
-      image: europeMap,
-    },
-    {
-      name: "North America",
-      countries: getCountryByContinent(countries as Country[], "North America"),
-      count: getCountryByContinent(countries as Country[], "North America")
-        .length,
-      image: northAmericaMap,
-    },
-    {
-      name: "South America",
-      countries: getCountryByContinent(countries as Country[], "South America"),
-      count: getCountryByContinent(countries as Country[], "South America")
-        .length,
-      image: southAmericaMap,
-    },
-    {
-      name: "Oceania",
-      countries: getCountryByContinent(countries as Country[], "Oceania"),
-      count: getCountryByContinent(countries as Country[], "Oceania").length,
-      image: oceaniaMap,
-    },
-  ];
+    if (countryData?.result) {
+      setCountries(countryData?.result as Country[]);
+    }
+  }, [destinationData, countryData]);
 
-  const featuredDestinations = getFeaturedDestinations(
-    destinations as Destination[],
+  const continents = useMemo(
+    () =>
+      countryData?.result
+        ? [
+            {
+              name: "Asia",
+              countries: getCountryByContinent(countries, "Asia"),
+              count: getCountryByContinent(countries, "Asia").length,
+              image: asiaMap,
+            },
+            {
+              name: "Africa",
+              countries: getCountryByContinent(countries, "Africa"),
+              count: getCountryByContinent(countries, "Africa").length,
+              image: africaMap,
+            },
+            {
+              name: "Europe",
+              countries: getCountryByContinent(countries, "Europe"),
+              count: getCountryByContinent(countries, "Europe").length,
+              image: europeMap,
+            },
+            {
+              name: "North America",
+              countries: getCountryByContinent(countries, "North America"),
+              count: getCountryByContinent(countries, "North America").length,
+              image: northAmericaMap,
+            },
+            {
+              name: "South America",
+              countries: getCountryByContinent(countries, "South America"),
+              count: getCountryByContinent(countries, "South America").length,
+              image: southAmericaMap,
+            },
+            {
+              name: "Oceania",
+              countries: getCountryByContinent(countries, "Oceania"),
+              count: getCountryByContinent(countries, "Oceania").length,
+              image: oceaniaMap,
+            },
+          ]
+        : [],
+    [countryData],
+  );
+
+  const featuredDestinations = useMemo(
+    () => (destinations ? getFeaturedDestinations(destinations) : []),
+    [destinations],
   );
 
   // Handle select state
-  const [selectedContinent, setSelectedContinent] = useState("Asia");
 
   function handleSelectContinent(): React.ReactEventHandler<HTMLSelectElement> {
     return (event) => {
@@ -108,11 +110,40 @@ const Discover: React.FC = () => {
 
   // Handle destination filter
   const filter = {
-    country: [],
+    locations: [],
     tags: [],
-  }
+  };
 
-  // Render
+  const countryNames = Array.isArray(countryData?.result)
+    ? (countryData.result as Country[]).map((country) => country?.name)
+    : [];
+  const continentNames = Array.isArray(continents)
+    ? continents.map((continent) => continent.name)
+    : [];
+  const filterLocations = [...countryNames, ...continentNames];
+
+  const filterTags = [
+    "Wilderness",
+    "Culture&Heritage",
+    "Food&Drink",
+    "SoloJourneys",
+    "CityScape",
+    "Season&Festival",
+    "Relaxation",
+  ];
+
+  const openFilterBoard = () => {
+    const filterBoard = document.querySelector(".filter-board");
+    if (filterBoard) {
+      return () => {
+        filterBoard.classList.toggle("hidden");
+        filterBoard.classList.toggle("flex");
+      };
+    }
+  };
+
+  // Handle navigating
+  const navigate = useNavigate();
 
   return (
     <main className="discover">
@@ -122,11 +153,14 @@ const Discover: React.FC = () => {
         featuredDestinations.length !== 0 && (
           <section className="posters h-screen">
             <div
-              className="poster px-sect relative flex w-full flex-col gap-0 py-sect-short"
+              className="poster px-sect relative flex w-full cursor-pointer flex-col gap-0 py-sect-short"
               style={{
                 backgroundImage: `url(${featuredDestinations[0].images[5]})`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
+              }}
+              onClick={() => {
+                navigate(`destinations/${featuredDestinations[0]._id}`);
               }}
             >
               <div className="blur-blob blob-1 absolute z-0"></div>
@@ -138,8 +172,8 @@ const Discover: React.FC = () => {
                   <span className="span-medium text-text-dark">
                     {featuredDestinations[0].country}
                   </span>
-                  <span className="span-medium text-text-dark">
-                    {featuredDestinations[0].tags[0]}
+                  <span className="span-medium mr-2 text-text-dark">
+                    {featuredDestinations[0].continent}
                   </span>
                 </div>
               </div>
@@ -186,23 +220,17 @@ const Discover: React.FC = () => {
                   </div>
 
                   <div className="flex flex-wrap justify-between gap-12">
-                    {continent.countries.map((country) => (
-                      <div key={country.name} className="flex flex-row gap-4">
-                        <div className="flex items-center justify-center rounded-xl bg-gray lg:h-20 lg:w-32">
-                          <img
-                            src={country?.images?.flagImage}
-                            alt={country.name}
-                            className="h-full w-full rounded-xl"
-                          />
-                        </div>
-                        <div className="flex flex-col">
-                          <span className="span-medium">{country.name}</span>
-                          <span className="span-regular">
-                            {country.totalDestinations} destinations
-                          </span>
-                        </div>
-                      </div>
-                    ))}
+                    {countryLoading && <p className="p-regular">Loading...</p>}
+                    {countryError && (
+                      <p className="p-regular">
+                        Please reload the page or <br /> try again later.
+                      </p>
+                    )}
+                    {!countryLoading &&
+                      !countryError &&
+                      continent.countries.map((country) => (
+                        <CountryCard country={country} key={country._id} />
+                      ))}
                   </div>
                 </div>
               );
@@ -213,23 +241,99 @@ const Discover: React.FC = () => {
       </section>
 
       {/* RELATED ARTICLES SECTION */}
-      <section className="related"></section>
+      <section className="related px-sect flex flex-col">
+        <h2 className="h2-md">Related articles</h2>
+      </section>
 
       {/* DESTINATION SECTION */}
-      <section className="destinations px-sect flex flex-col items-center">
+      <section className="destinations px-sect flex flex-col items-center py-sect-default">
         <h1 className="h1-md">Discover destinations</h1>
-        <div className="flex flex-row justify-between w-full py-sect-short">
+        <div className="flex w-full flex-row justify-between py-sect-short">
           <p className="p-medium">
             Each destination we’ve covered here is fully filled <br />
             with significant information you will need
           </p>
-          <button
-            title="filter"
-            className="rounded-full bg-background-dark shadow-section lg:h-12 lg:w-12 xl:h-12 xl:w-12 2xl:h-16 2xl:w-16 3xl:h-16 3xl:w-16"
-          >
-            <i className="ri-filter-3-line p-large text-text-dark m-auto"></i>
-          </button>
+          <div className="relative">
+            <button
+              title="filter"
+              className="rounded-full bg-background-dark shadow-component lg:h-12 lg:w-12 xl:h-12 xl:w-12 2xl:h-16 2xl:w-16 3xl:h-16 3xl:w-16"
+              onClick={openFilterBoard()}
+            >
+              <i className="ri-filter-3-line p-large m-auto text-text-dark"></i>
+            </button>
+            <div className="filter-board absolute right-5p top-2/3 z-10 hidden w-0.4svw flex-col items-center gap-8 rounded-xl bg-background-light px-4 pb-20 pt-4 shadow-section">
+              <div className="flex w-full flex-row items-end gap-4">
+                <div className="flex h-fit items-center justify-between rounded-md px-2 py-1 lg:border 2xl:border-2">
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    className="p-regular w-4/5 bg-transparent outline-none"
+                  />
+                  <button title="search" className="outline-none">
+                    <i className="ri-search-line span-regular"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="flex w-full flex-col items-start gap-2">
+                <span className="span-regular uppercase">Location</span>
+                <div className="flex flex-wrap gap-2">
+                  {filterLocations.map((location) => (
+                    <span
+                      key={location}
+                      className="span-small rounded-2xl border-solid border-text-light px-4 lg:border 2xl:border-2"
+                    >
+                      {location}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex w-full flex-col items-start gap-2">
+                <span className="span-regular uppercase">Tags</span>
+                <div className="flex flex-wrap gap-2">
+                  {filterTags.map((tag) => (
+                    <span
+                      key={tag}
+                      className="span-small rounded-2xl border-solid border-text-light px-4 lg:border 2xl:border-2"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="grid w-full auto-cols-1/3 grid-flow-col gap-8">
+          {destinationLoading && <p>Loading...</p>}
+          {destinationError && (
+            <p>
+              Please reload the page or <br /> try again later.
+            </p>
+          )}
+          {destinations &&
+            (destinations as Destination[])?.map((destination) => (
+              <DestinationCard
+                destination={destination}
+                key={destination._id}
+              />
+            ))}
+        </div>
+        {destinations && (
+          <Pagination
+            count={totalDestinations}
+            page={currentPage}
+            limit={18}
+            handlePreviousClick={() =>
+              setCurrentPage(Math.max(1, currentPage - 1))
+            }
+            handlePageClick={(page) => {
+              console.log(page);
+              setCurrentPage(page);
+            }}
+            handleNextClick={() => setCurrentPage(currentPage + 1)}
+          />
+        )}
       </section>
     </main>
   );
