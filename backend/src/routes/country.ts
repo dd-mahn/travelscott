819c74315b2c39d25aa322ad860fd46d6425b87b
dd-mahn <ducmanh0801@gmail.com>
2024-07-17 14:20:09 +0700

    feat: Add endpoint to update country images

--- backend/src/routes/country.ts
+++ backend/src/routes/country.ts
@@ -5,6 +5,7 @@ import {
   getCountries,
   getCountryById,
   updateCountry,
+  updateCountryImages,
   updateTotalDestinations,
 } from "src/controllers/CountryControllers";
 
@@ -13,6 +14,7 @@ const router = express.Router();
 router.get("/", getCountries);
 router.post("/", createCountry);
 router.get("/:id", getCountryById);
+router.put("/:id/images", updateCountryImages)
 router.put("/:id", updateCountry);
 router.put("/:id/total", updateTotalDestinations);
 router.delete("/:id", deleteCountry);
