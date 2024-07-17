188b2a8af862fe82dc9eda99d4ce45ff0be0135b
dd-mahn <ducmanh0801@gmail.com>
2024-07-17 14:20:24 +0700

    refactor: Update AWS configuration to use environment variables

--- backend/src/utils/aws.ts
+++ backend/src/utils/aws.ts
@@ -1,4 +1,11 @@
 import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env.aws' });
+
+console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
+console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
+console.log('AWS_REGION:', process.env.AWS_REGION);
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
