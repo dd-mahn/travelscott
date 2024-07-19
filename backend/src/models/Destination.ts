f2f328603a875b1128a2831875856208317b2fb6
dd-mahn <ducmanh0801@gmail.com>
2024-07-20 01:19:09 +0700

    refactor: Update Destination model with additional fields

--- backend/src/models/Destination.ts
+++ backend/src/models/Destination.ts
@@ -1,61 +1,79 @@
 import mongoose from "mongoose";
 
 const destinationSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  video: {
+    type: String,
+    default: "",
+  },
+  images: {
+    type: [],
+    default: [],
+  },
+  country: {
+    type: String,
+    default: "",
+  },
+  continent: {
+    type: String,
+    default: "",
+  },
+  location: {
+    type: String,
+    default: "",
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  places: {
+    type: Object,
+    default: { to_stay: [], to_visit: [], to_eat: [] },
+  },
+  transportation: {
+    type: Object,
+    default: { overview: "", types: [] },
+  },
+  additionalInfo: {
+    type: Object,
+    default: {
+      whenToVisit: "",
+      whoToGoWith: "",
+      whatToExpect: "",
+      healthAndSafety: "",
     },
-    video:{
-        type: String,
-        default: '',
-    },
-    images: {
-        type: [],
-        default: [],
-    },
-    country: {
-        type: String,
-        default: ""
-    },
-    continent: {
-        type: String,
-        default: "",
-    },
-    location: {
-        type: String,
-        default: '',
-    },
-    description: {
-        type: String,
-        default: '',
-    },
-    places: {
-        type: Object,
-        default: { _dummy: null },
-    },
-    transportation: {
-        type: Object,
-        default: { _dummy: null },
-    },
-    tags: {
-        type: [String],
-        default: [],
-        enum: ["Wilderness", "Culture&Heritage", "Food&Drink", "SoloJourneys", "CityScape", "Season&Festival", "Relaxation", "FirstTimeAbroad"]
-    },
-    insight: {
-        type: Object,
-        default: { _dummy: null },
-    },
-    summary: {
-        type: String,
-        default: '',
-    },
-    featured: {
-        type: Boolean,
-        default: false,
-    }
+  },
+  tags: {
+    type: [String],
+    default: [],
+    enum: [
+      "Wilderness",
+      "Culture&Heritage",
+      "Food&Drink",
+      "SoloJourneys",
+      "CityScape",
+      "Season&Festival",
+      "Relaxation",
+      "FirstTimeAbroad",
+    ],
+  },
+  insight: {
+    type: Object,
+    default: { fromUs: [], fromOthers: [] },
+  },
+  summary: {
+    type: String,
+    default: "",
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-const Destination = mongoose.model('Destination', destinationSchema);
+const Destination = mongoose.model("Destination", destinationSchema);
 
 export default Destination;
