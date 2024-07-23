fc97b7a85462a2491121de18ff3fc1d52aba4e12
dd-mahn <ducmanh0801@gmail.com>
2024-07-24 00:56:00 +0700

    feat: Update Routers to include Inspiration and Article pages

--- frontend/src/router/Routers.tsx
+++ frontend/src/router/Routers.tsx
@@ -3,11 +3,12 @@ import { Routes, Route } from "react-router-dom";
 
 import About from "src/pages/About";
 import Contact from "src/pages/Contact";
-import Test from "src/pages/Test";
+import Inspiration from "src/pages/Inspiration";
 import Home from "src/pages/Home";
 import Discover from "src/pages/Discover";
 import Country from "src/pages/Country";
 import DestinationPage from "src/pages/Destination";
+import Article from "src/pages/Article";
 
 
 const Routers = () => {
@@ -19,6 +20,8 @@ const Routers = () => {
       <Route path="/discover" element={<Discover />} />
       <Route path="/discover/countries/:id" element={<Country />} />
       <Route path="/discover/destinations/:id" element={<DestinationPage/>}/>
+      <Route path="/inspiration" element={<Inspiration />} />
+      <Route path="/inspiration/:id" element={<Article />} />
     </Routes>
   );
 };
