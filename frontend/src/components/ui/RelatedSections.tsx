54fcd8dc611e2981e8966e5202b0fbb06b328e90
dd-mahn <ducmanh0801@gmail.com>
2024-07-25 01:08:03 +0700

    Add 1 more level to type checking logic in RelatedBlogs

--- frontend/src/components/ui/RelatedSections.tsx
+++ frontend/src/components/ui/RelatedSections.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import useFetch from "src/hooks/useFetch";
 import Blog from "src/types/Blog";
 import Country from "src/types/Country";
 import Destination from "src/types/Destination";
-import { FetchBlogsType, FetchCountriesType, FetchDestinationType } from "src/types/FetchData";
+import {
+  FetchBlogsType,
+  FetchCountriesType,
+  FetchDestinationType,
+} from "src/types/FetchData";
 import { BASE_URL } from "src/utils/config";
 
 type RelatedSectionsProps = {
@@ -16,8 +21,10 @@ const RelatedSections: React.FC<RelatedSectionsProps> = ({ type, data }) => {
     <RelatedCountries country={data as Country} />
   ) : type === "destination" ? (
     <RelatedDestinations destination={data as Destination} />
-  ) : (
+  ) : type === "blog" ? (
     <RelatedArticles data={data} />
+  ) : (
+    <p>No related</p>
   );
 };
 
@@ -61,18 +68,20 @@ const RelatedCountries: React.FC<CountryProps> = ({ country }) => {
     );
 
   return (
-    <div className="min-w-screen flex flex-nowrap gap-0 py-sect-short">
+    <div className="related-countries min-w-screen flex flex-nowrap gap-0 py-sect-short">
       {relatedCountries.map((country) => (
         <div
           key={country.name}
           className="grid h-0.3svh w-0.2svw place-items-center bg-background-dark bg-opacity-20"
           style={{
-            backgroundImage: `url(${country.images.otherImages?.[0]})`,
+            backgroundImage: `linear-gradient(rgba(0,0,0,0.4) , rgba(0,0,0,0.4)), url(${country.images.otherImages?.[0]})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-          <h3 className="h3-md text-text-dark">{country.name}</h3>
+          <p className="p-large text-center font-prima text-text-dark">
+            {country.name}
+          </p>
         </div>
       ))}
     </div>
@@ -87,43 +96,47 @@ const RelatedDestinations: React.FC<DestinationProps> = ({ destination }) => {
     data: destinationsData,
     error: destinationsError,
     loading: destinationsLoading,
-  } = useFetch<FetchDestinationType>(
-    `${BASE_URL}/destinations?limit=100`,
-  );
+  } = useFetch<FetchDestinationType>(`${BASE_URL}/destinations?limit=100`);
 
   const continentRelatedDestinations = destinationsData?.result.filter(
-    (d) => d.continent === continent,
+    (d) => d.continent === continent && d._id !== destination._id,
   );
 
   const tagsRelatedDestinations = destinationsData?.result.filter(
-    (d) => d._id !== destination._id && d.tags.some((tag) => tags.includes(tag)),
+    (d) =>
+      d._id !== destination._id && d.tags.some((tag) => tags.includes(tag)),
   );
 
-  const relatedDestinations = [...(continentRelatedDestinations || []), ...(tagsRelatedDestinations || [])];
+  const relatedDestinations = [
+    ...(continentRelatedDestinations || []),
+    ...(tagsRelatedDestinations || []),
+  ];
 
   if (destinationsLoading) return <div>Loading...</div>;
   if (destinationsError) return <div>Error: {destinationsError}</div>;
   if (!relatedDestinations || relatedDestinations.length === 0)
     return (
-      <div className="px-sect">
+      <div className="px-sect grid place-items-center">
         <p className="p-regular mt-4">
           There are no related destinations at the moment
         </p>
       </div>
     );
   return (
-    <div className="min-w-screen flex flex-nowrap gap-0 py-sect-short">
+    <div className="related-destinations min-w-screen flex flex-nowrap gap-2 py-sect-short">
       {relatedDestinations.map((destination) => (
         <div
           key={destination.name}
           className="grid h-0.3svh w-0.2svw place-items-center bg-background-dark bg-opacity-20"
           style={{
-            backgroundImage: `url(${destination.images?.[0]}})`,
+            backgroundImage: `linear-gradient(rgba(0,0,0,0.4) , rgba(0,0,0,0.4)), url(${destination.images?.[0]})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-          <h3 className="h3-md text-text-dark">{destination.name}</h3>
+          <p className="p-large text-center font-prima text-text-dark">
+            {destination.name}
+          </p>
         </div>
       ))}
     </div>
@@ -131,6 +144,8 @@ const RelatedDestinations: React.FC<DestinationProps> = ({ destination }) => {
 };
 
 const RelatedArticles: React.FC<BlogProps> = ({ data }) => {
+  const navigate = useNavigate();
+
   const continents = [
     "Africa",
     "Asia",
@@ -139,24 +154,34 @@ const RelatedArticles: React.FC<BlogProps> = ({ data }) => {
     "Oceania",
     "South America",
   ];
-  // Improved type handling using type guards
-  const isBlog = (data: any): data is Blog => data && data.category;
-  const isDestination = (data: any): data is Destination => data && data._id;
-  const isCountry = (data: any): data is Country => data && data.name;
-  const isContinent = (data: any): data is string => continents.includes(data);
   // Fetch related blogs
   const {
     data: blogData,
     error: blogError,
     loading: blogLoading,
-  } = useFetch<FetchBlogsType>(`${BASE_URL}/blogs/limit=100`);
+  } = useFetch<FetchBlogsType>(`${BASE_URL}/blogs?limit=100`);
   const blogs = blogData?.result;
 
+  // Handle type checking
+  const isBlog = (data: any): data is Blog => data && data.category;
+  const isDestination = (data: any): data is Destination => data && data._id;
+  const isCountry = (data: any): data is Country => data && data.name;
+  const isContinent = (data: any): data is string => continents.includes(data);
+
+  const checkDataType = (data: any): string => {
+    if (isBlog(data)) return "Blog";
+    if (isDestination(data)) return "Destination";
+    if (isCountry(data)) return "Country";
+    if (isContinent(data)) return "Continent";
+    return "Unknown";
+  };
+
+  // Render logic
   if (blogLoading) return <div>Loading...</div>;
   if (blogError) return <div>Error: {blogError}</div>;
   if (!blogs) {
     return (
-      <div className="px-sect">
+      <div className="">
         <p className="p-regular mt-4">
           There are no related articles at the moment
         </p>
@@ -164,38 +189,59 @@ const RelatedArticles: React.FC<BlogProps> = ({ data }) => {
     );
   }
 
+  // Handle cases for rendering
   let relatedBlogs = blogs;
 
-  if (isBlog(data)) {
+  if (isBlog(data) && checkDataType(data) === "Blog") {
+    console.log("isBlog");
     relatedBlogs = relatedBlogs.filter(
-      (blog) => blog.category !== data.category,
+      (blog) =>
+        blog._id !== data._id &&
+        (blog.category === data.category ||
+          data.tags.some((tag) => blog.tags.includes(tag))),
     );
   }
-  if (isDestination(data)) {
+  if (isDestination(data) && checkDataType(data) === "Destination") {
+    console.log("isDestination");
     relatedBlogs = relatedBlogs.filter(
-      (blog) => blog.related_destination !== data._id,
+      (blog) => blog.related_destination === data._id,
     );
   }
-  if (isCountry(data)) {
+  if (isCountry(data) && checkDataType(data) === "Country") {
+    console.log("isCountry");
     relatedBlogs = relatedBlogs.filter((blog) => blog.tags.includes(data.name));
   }
-  if(isContinent(data)) {
+  if (isContinent(data) && checkDataType(data) === "Continent") {
+    console.log("isContinent");
     relatedBlogs = relatedBlogs.filter((blog) => blog.tags.includes(data));
   }
 
+  if (relatedBlogs.length === 0) {
+    return (
+      <div className="">
+        <p className="p-regular mt-4">
+          There are no related articles at the moment
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div className="min-w-screen flex flex-nowrap gap-0 py-sect-short">
+    <div className="related-blogs min-w-full flex flex-nowrap gap-2 py-sect-short">
       {relatedBlogs.map((blog) => (
         <div
           key={blog.title}
-          className="grid h-0.3svh w-0.2svw place-items-center bg-background-dark bg-opacity-20"
+          className="grid h-0.3svh w-0.2svw cursor-pointer place-items-center"
           style={{
-            backgroundImage: `url(${blog.image}})`,
+            backgroundImage: `linear-gradient(rgba(0,0,0,0.4) , rgba(0,0,0,0.4)), url(${blog.image})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
+          onClick={() => navigate(`/inspiration/${blog._id}`)}
         >
-          <h3 className="h3-md text-text-dark">{blog.title}</h3>
+          <p className="p-large text-center font-prima text-text-dark">
+            {blog.title}
+          </p>
         </div>
       ))}
     </div>
