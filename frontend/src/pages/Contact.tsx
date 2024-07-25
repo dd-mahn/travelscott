d867c285e0496c380caab8c3c5e675b29c235345
dd-mahn <ducmanh0801@gmail.com>
2024-07-25 17:48:14 +0700

    Refactor Contact page to use StyledInput for form fields

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import planeIcon from "src/assets/svg/plane-icon.svg";
+import StyledInput from "src/components/ui/StyledInput";
 import "src/styles/contact.css";
 
 const Contact: React.FC = () => {
@@ -20,7 +21,7 @@ const Contact: React.FC = () => {
             Looking to collaborate with us commercially?
           </h2>
           <button
-            className={`${visibleSection === "emailing" ? "rotate-180" : ""} rounded-full border lg:h-20 lg:w-20 xl:h-24 xl:w-24 2xl:h-28 2xl:w-28 3xl:h-28 3xl:w-28`}
+            className={`${visibleSection === "emailing" ? "rotate-180" : ""} transition-all rounded-full border lg:h-20 lg:w-20 xl:h-24 xl:w-24 2xl:h-28 2xl:w-28 3xl:h-28 3xl:w-28`}
             title="open btn"
             onClick={() => {
               toggleInfo("emailing");
@@ -52,7 +53,7 @@ const Contact: React.FC = () => {
         <div className="flex flex-row items-center justify-between border-b pb-8">
           <h2 className="h2-md">Want to share your experience as resource?</h2>
           <button
-            className={`${visibleSection === "contribute" ? "rotate-180" : ""} rounded-full border lg:h-20 lg:w-20 xl:h-24 xl:w-24 2xl:h-28 2xl:w-28 3xl:h-28 3xl:w-28`}
+            className={`${visibleSection === "contribute" ? "rotate-180" : ""} transition-all rounded-full border lg:h-20 lg:w-20 xl:h-24 xl:w-24 2xl:h-28 2xl:w-28 3xl:h-28 3xl:w-28`}
             title="open btn"
             onClick={() => {
               toggleInfo("contribute");
@@ -79,7 +80,7 @@ const Contact: React.FC = () => {
         <div className="flex flex-row items-center justify-between border-b pb-8">
           <h2 className="h2-md">Want to give us a feedback?</h2>
           <button
-            className={`${visibleSection === "feedback" ? "rotate-180" : ""} rounded-full border lg:h-20 lg:w-20 xl:h-24 xl:w-24 2xl:h-28 2xl:w-28 3xl:h-28 3xl:w-28`}
+            className={`${visibleSection === "feedback" ? "rotate-180" : ""} transition-all rounded-full border lg:h-20 lg:w-20 xl:h-24 xl:w-24 2xl:h-28 2xl:w-28 3xl:h-28 3xl:w-28`}
             title="open btn"
             onClick={() => {
               toggleInfo("feedback");
@@ -101,52 +102,14 @@ const Contact: React.FC = () => {
           </p>
 
           <form action="" className="flex w-2/5 flex-col gap-12 pt-8">
-            <div className="relative w-full">
-              <label
-                className="p-regular absolute bottom-0"
-                htmlFor="first-name"
-              >
-                {" "}
-                First name
-              </label>
-              <input className="p-regular" type="text" id="first-name"></input>
-            </div>
-
-            <div className="relative w-full">
-              <label
-                className="p-regular absolute bottom-0"
-                htmlFor="last-name"
-              >
-                Last name
-              </label>
-              <input className="p-regular" type="text" id="last-name"></input>
-            </div>
-
-            <div className="relative w-full">
-              <label className="p-regular absolute bottom-0" htmlFor="email">
-                {" "}
-                Email address
-              </label>
-              <input className="p-regular" type="email" id="email"></input>
-            </div>
-
-            <div className="relative w-full">
-              <label className="p-regular absolute bottom-0" htmlFor="age">
-                {" "}
-                Age
-              </label>
-              <input className="p-regular" type="number" id="age"></input>
-            </div>
-
-            <div className="relative w-full">
-              <label className="p-regular absolute bottom-0" htmlFor="country">
-                Country
-              </label>
-              <input className="p-regular" type="text" id="country"></input>
-            </div>
+            <StyledInput type="text" id="first-name" label="First name" />
+            <StyledInput type="text" id="last-name" label="Last name" />
+            <StyledInput type="email" id="email" label="Email address" />
+            <StyledInput type="text" id="age" label="Age" />
+            <StyledInput type="text" id="country" label="Country" />
 
             <div className="flex w-full flex-col">
-              <label htmlFor="message" className="p-regular">
+              <label htmlFor="message" className="span-regular">
                 What you want to tell us
               </label>
               <textarea
