99ce5cf3559604cca7d607558228814ad0b32562
dd-mahn <ducmanh0801@gmail.com>
2024-07-27 01:36:10 +0700

    Refactor Feedback model to include firstName, lastName, age, and country fields

--- backend/src/models/Feedback.ts
+++ backend/src/models/Feedback.ts
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 
 const feedbackSchema = new mongoose.Schema({
-    name: {
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName:{
         type: String,
         required: true
     },
     email:{
+        type: String,
+        required: true
+    },
+    age:{
+        type: Number,
+        required: true
+    },
+    country:{
         type: String,
         required: true
     },
