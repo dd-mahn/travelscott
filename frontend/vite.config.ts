86bed98fcb59d532e18bf93c4afebde26080d161
dd-mahn <ducmanh0801@gmail.com>
2024-08-08 01:17:13 +0700

    Add vite-image-optimizer config for image optimizing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 50,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 50,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 50,
+      },
+      tiff: {
+        // https://sharp.pixelplumbing.com/api-output#tiff
+        quality: 50,
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      'src': '/src'
-    }
-  }
-})
+      src: "/src",
+    },
+  },
+});
