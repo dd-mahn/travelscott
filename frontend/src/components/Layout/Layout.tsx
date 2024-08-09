361775cbd8323be473a5c8015f452b3b1645a0d8
dd-mahn <ducmanh0801@gmail.com>
2024-08-09 16:33:48 +0700

    Add loading screen to root path

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,11 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "src/components/Header/Header";
 import Footer from "src/components/Footer/Footer";
 import Routers from "src/router/Routers";
 import { useLocation } from "react-router-dom";
+import Loading from "src/pages/Loading";
 
 const Layout = () => {
   const location = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    } else setLoading(false);
+
+    return () => {
+      setLoading(true);
+    };
+  }, [location]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -13,9 +27,15 @@ const Layout = () => {
 
   return (
     <>
-      <Header />
-      <Routers />
-      <Footer />
+      {loading ? (
+        <Loading/>
+      ) : (
+        <>
+          <Header />
+          <Routers />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
