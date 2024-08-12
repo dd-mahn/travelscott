0476f48faf46dcfcd78a0b001e5a1a6ea4a32551
dd-mahn <ducmanh0801@gmail.com>
2024-08-13 01:55:15 +0700

    Add conditions for showing Loading page once only

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { ReactLenis } from "lenis/dist/lenis-react";
+import { useLocation } from "react-router-dom";
+
 import Header from "src/components/Header/Header";
 import Footer from "src/components/Footer/Footer";
 import Routers from "src/router/Routers";
-import { useLocation } from "react-router-dom";
-import Loading from "src/pages/Loading";
+import Loading from "src/components/ui/Loading";
 
 const Layout = () => {
   const location = useLocation();
   const [loading, setLoading] = useState(true);
+  const [loadingShown, setLoadingShown] = useState(false);
 
   useEffect(() => {
     if (location.pathname === "/") {
       setTimeout(() => {
         setLoading(false);
+        setLoadingShown(true);
       }, 3000);
     } else setLoading(false);
 
@@ -22,18 +26,20 @@ const Layout = () => {
   }, [location]);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
+    window.scrollTo({top: 0});
+  }, [location.pathname]);
 
   return (
     <>
-      {loading ? (
-        <Loading/>
+      {loading && !loadingShown ? (
+        <Loading />
       ) : (
         <>
-          <Header />
-          <Routers />
-          <Footer />
+          <ReactLenis root options={{ lerp: 0.08 }}>
+            <Header />
+            <Routers />
+            <Footer />
+          </ReactLenis>
         </>
       )}
     </>
