689c344479f0afe34a2a83cc488bffce38b6b7c5
dd-mahn <ducmanh0801@gmail.com>
2024-08-16 02:11:31 +0700

    refactor: Add AnimatedLogoScreen component for displaying animated logo on screen

---
+++ frontend/src/components/ui/AnimatedLogoScreen.tsx
@@ -0,0 +1,24 @@
+import React, { memo } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import StaggerLogo from "./staggerLogo";
+
+const AnimatedLogoScreen: React.FC = () => {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key="WaitScreen"
+        initial="visible"
+        animate="visible"
+        exit={{ y: -1000 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className="grid h-screen w-screen place-items-center bg-background-light"
+      >
+        <div className="w-screen select-none overflow-hidden text-center font-logo text-text-light lg:mr-16 lg:pr-12 lg:text-8xl xl:text-13xl 2xl:pr-20 2xl:text-14xl 3xl:text-15xl">
+          <StaggerLogo />
+        </div>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default memo(AnimatedLogoScreen);
