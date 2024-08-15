d6552295b05f43e117f6779d8c170eed237f74c2
dd-mahn <ducmanh0801@gmail.com>
2024-08-16 02:11:47 +0700

    refactor: Simplify Loading component

--- frontend/src/components/ui/Loading.tsx
+++ frontend/src/components/ui/Loading.tsx
@@ -1,34 +1,11 @@
-import React, { memo } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import StaggerLogo from "./staggerLogo";
-
-const variants = {
-  hidden: { opacity: 0, y: 50 },
-  hiddenY: (y: string) => {
-    return {
-      y: y,
-    };
-  },
-  visible: { opacity: 1, y: 0 },
-};
+import React from "react";
 
 const Loading: React.FC = () => {
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key="WaitScreen"
-        initial="visible"
-        animate="visible"
-        exit={{ y: -1000 }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
-        className="grid h-screen w-screen place-items-center bg-background-light"
-      >
-        <div className="w-screen select-none overflow-hidden text-center font-logo text-text-light lg:mr-16 lg:pr-12 lg:text-8xl xl:text-13xl 2xl:pr-20 2xl:text-14xl 3xl:text-15xl">
-          <StaggerLogo />
-        </div>
-      </motion.div>
-    </AnimatePresence>
+    <div className="grid h-screen w-screen place-items-center">
+      <h1 className="h1-md">Loading...</h1>
+    </div>
   );
 };
 
-export default memo(Loading);
+export default Loading;
