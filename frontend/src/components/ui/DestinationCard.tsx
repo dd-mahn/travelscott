dce2fc52b12753978b807e60ac9e327e2772ce91
dd-mahn <ducmanh0801@gmail.com>
2024-08-16 16:44:02 +0700

    refactor: Optimize image loading in CountryCard and DestinationCard components

--- frontend/src/components/ui/DestinationCard.tsx
+++ frontend/src/components/ui/DestinationCard.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
 import Destination from "src/types/Destination";
+import planeIcon from "src/assets/svg/plane-icon.svg";
 
 interface DestinationCardProps {
   destination: Destination;
@@ -9,39 +12,62 @@ interface DestinationCardProps {
 const DestinationCard: React.FC<DestinationCardProps> = ({ destination }) => {
   const navigate = useNavigate();
   return (
-    <div
-      key={destination.name}
-      className="destination-card flex w-full cursor-pointer flex-col gap-2"
+    <Suspense
+      fallback={
+        <div className="grid h-[50svh] w-full place-items-center overflow-hidden rounded-xl bg-gradient-to-t from-background-dark to-transparent">
+          <img src={planeIcon} alt="" className="w-1/5" />
+        </div>
+      }
     >
       <div
-        className="relative flex h-[50svh] items-center justify-center rounded-xl bg-gray"
-        onClick={() => {
-          navigate(`/discover/destinations/${destination._id}`);
-        }}
+        key={destination.name}
+        className="destination-card flex w-full cursor-pointer flex-col gap-2"
       >
-        <img
-          src={destination.images[0]}
-          alt={destination.name}
-          className="h-full w-full rounded-xl"
-        />
-      </div>
-      <div className="flex h-fit flex-col justify-start gap-0">
-        <span className="span-regular text-gray">
-          {destination.country || "Country"}
-        </span>
-        <span className="span-medium uppercase">{destination.name}</span>
-        <div className="mt-4 flex flex-row items-start justify-start gap-2">
-          {destination.tags.map((tag) => (
-            <span
-              key={tag}
-              className="span-small rounded-2xl border-solid border-gray px-4 lg:border"
-            >
-              {tag}
-            </span>
-          ))}
+        <div
+          className="grid h-[50svh] place-items-center overflow-hidden rounded-xl bg-gradient-to-t from-background-dark to-transparent"
+          onClick={() => {
+            navigate(`/discover/destinations/${destination._id}`);
+          }}
+        >
+          {destination.images[0] && (
+            <motion.img
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.4 }}
+              loading="lazy"
+              src={destination.images[0]}
+              alt={destination.name}
+              className="h-full w-full rounded-xl"
+            />
+          )}
+        </div>
+        <div className="flex h-fit flex-col justify-start gap-0">
+          <span className="span-regular text-gray">
+            {destination.country || "Country"}
+          </span>
+          <motion.span
+            whileHover={{ x: 5 }}
+            transition={{
+              duration: 1,
+              type: "spring",
+              bounce: 0.5,
+            }}
+            className="span-medium w-fit uppercase"
+          >
+            {destination.name}
+          </motion.span>
+          <div className="mt-4 flex flex-row items-start justify-start gap-2">
+            {destination.tags.map((tag) => (
+              <span
+                key={tag}
+                className="span-small rounded-2xl border-solid border-gray px-4 lg:border"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
