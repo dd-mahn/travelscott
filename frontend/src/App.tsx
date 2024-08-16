52b449bfc6030fe27527b0e424a30a98e3289202
dd-mahn <ducmanh0801@gmail.com>
2024-08-16 16:50:54 +0700

    Add slick import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "src/App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Layout from "src/components/Layout/Layout";
 
 function App() {
