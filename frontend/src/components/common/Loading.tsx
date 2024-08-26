2717600b451ac4b26e78c62373d15baa5c061826
dd-mahn <ducmanh0801@gmail.com>
2024-08-27 00:35:58 +0700

    refactor: Update Loading component with framer-motion animation

--- frontend/src/components/common/Loading.tsx
+++ frontend/src/components/common/Loading.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Loading: React.FC = () => {
   return (
-    <div className="grid h-screen w-screen place-items-center">
-      <h1 className="h1-md">Loading...</h1>
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.2, type: "spring" }}
+        exit={{ opacity: 0 }}
+        className="grid h-screen w-screen place-items-center"
+      >
+        <h2 className="h2-md">Loading...</h2>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
