fb0b9691b035576fdc143aa6707e75388c15bf35
dd-mahn <ducmanh0801@gmail.com>
2024-08-27 00:37:41 +0700

    refactor: Optimize Inspired component with framer-motion animations, lazy loading, and accessibility improvements

--- frontend/src/pages/HomeComponents/Inspired.tsx
+++ frontend/src/pages/HomeComponents/Inspired.tsx
@@ -7,9 +7,8 @@ const variants = {
     opacity: 0,
     y: 20,
   },
-  hiddenY: {
-    opacity: 0,
-    y: 100,
+  hiddenFullY: {
+    y: "100%",
   },
   visible: {
     opacity: 1,
@@ -24,18 +23,19 @@ const Inspired: React.FC = () => {
     <section className="px-sect inspired flex items-center justify-center lg:pb-sect-semi lg:pt-sect-default 2xl:pb-sect-semi 2xl:pt-sect-medium">
       <div className="relative flex w-fit flex-col lg:gap-2 xl:gap-2 2xl:gap-4 3xl:gap-4">
         <div className="blob-brown blur-blob absolute -left-1/3 -top-[10%] z-0 h-full w-1/2 opacity-60"></div>
-        <motion.h1
+        <motion.h2
           variants={variants}
-          initial="hiddenY"
+          initial="hidden"
           whileInView="visible"
           viewport={{ once: true, margin: "-300px" }}
           transition={{ duration: 0.8 }}
-          className="h2-inter text-center leading-normal tracking-tight"
+          className="h2-inter text-center tracking-tight"
+          style={{ lineHeight: "1.2"}}
         >
           We got inspired by travelers of <br />
           <span className="text-main-green">20</span>+ countries around the
           world
-        </motion.h1>
+        </motion.h2>
         <motion.span
           initial="hidden"
           whileInView="visible"
