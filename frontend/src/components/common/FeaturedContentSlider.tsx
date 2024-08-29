3c6c475ba561eff21d5a356dbdbf2df46fa58e8c
dd-mahn <ducmanh0801@gmail.com>
2024-08-30 02:43:54 +0700

    refactor: Add FeaturedContentSlider component for optimized content sliding and pagination

---
+++ frontend/src/components/common/FeaturedContentSlider.tsx
@@ -0,0 +1,100 @@
+import React, { memo, useCallback, useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { DotPagination } from "./Pagination";
+
+// Framer motion variants
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
+type FeaturedContentSliderProps = {
+  children: React.ReactNode[];
+};
+
+const FeaturedContentSlider: React.FC<FeaturedContentSliderProps> = ({
+  children,
+}) => {
+  const [direction, setDirection] = useState(1);
+  const [index, setIndex] = useState(0);
+  const childRef = React.useRef<HTMLDivElement>(null);
+  const containerRef = React.useRef<HTMLDivElement>(null);
+
+  const handleNext = useCallback(() => {
+    if (index < children.length - 1) {
+      setDirection(1);
+      setIndex(index + 1);
+    }
+  }, [index, children.length]);
+
+  const handlePrev = useCallback(() => {
+    if (index > 0) {
+      setDirection(-1);
+      setIndex(index - 1);
+    }
+  }, [index]);
+
+  useEffect(() => {
+    if (containerRef.current && childRef.current) {
+      containerRef.current.style.height = `${childRef.current.offsetHeight}px`;
+    }
+  }, [index]);
+
+  return (
+    <>
+      <div ref={containerRef} className="relative w-full overflow-hidden">
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.div
+            ref={childRef}
+            key={index}
+            custom={direction}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+            variants={variants}
+            className="absolute left-0 top-0 h-fit w-full"
+          >
+            {children[index]}
+          </motion.div>
+        </AnimatePresence>
+      </div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+        variants={variants}
+        className="flex w-full justify-center"
+      >
+        <DotPagination
+          count={children.length}
+          index={index}
+          handleNextClick={handleNext}
+          handlePreviousClick={handlePrev}
+        />
+      </motion.div>
+    </>
+  );
+};
+
+export default memo(FeaturedContentSlider);
--- frontend/src/components/common/featuredBlogs.tsx
+++ frontend/src/components/common/featuredBlogs.tsx
@@ -1,69 +1,69 @@
-import React, { useEffect, useState } from "react";
-import Blog from "src/types/Blog";
-import { DotPagination } from "./Pagination";
+import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+import Blog from "src/types/Blog";
+import FeaturedContentSlider from "./FeaturedContentSlider";
+import { formatDate } from "src/utils/formatDate";
 
 type featuredBlogsProps = {
   blogs: Blog[];
 };
 
+const variants = {
+  hoverScale: {
+    scale: 1.05,
+    transition: {
+      duration: 0.4,
+    },
+  },
+
+  tapScale: {
+    scale: 0.95,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
 const FeaturedBlogs: React.FC<featuredBlogsProps> = ({ blogs }) => {
-  const [currentBlogIndex, setCurrentBlogIndex] = useState<number>(0);
-  const [currentBlog, setCurrentBlog] = useState<Blog>(blogs[currentBlogIndex]);
-
-  useEffect(() => {
-    setCurrentBlog(blogs[currentBlogIndex]);
-  }, [currentBlogIndex]);
-
-  const handleNextBlog = () => {
-    if (currentBlogIndex < blogs.length - 1) {
-      setCurrentBlogIndex(currentBlogIndex + 1);
-    }
-  };
-
-  const handlePrevBlog = () => {
-    if (currentBlogIndex > 0) {
-      setCurrentBlogIndex(currentBlogIndex - 1);
-    }
-  };
-
   return (
     <div className="flex flex-col items-center gap-8 pb-sect-default">
-      <Link
-        to={`/inspiration/${currentBlog._id}`}
-        target="_top"
-        className="px-sect flex w-svw cursor-pointer flex-col items-center gap-8"
-      >
-        <div
-          className="h-[75svh] w-full rounded-xl shadow-section"
-          style={{
-            backgroundImage: `url(${currentBlog?.image})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className="flex flex-col items-center gap-2">
-          <span className="span-regular text-gray">
-            {currentBlog?.category}
-          </span>
-          <h2 className="h2-md"> {currentBlog?.title}</h2>
-        </div>
-        <p className="p-regular w-2/5 text-center">
-          {" "}
-          {currentBlog?.content?.[0]?.sectionText?.[0]}
-        </p>
-        <span className="span-regular flex items-center gap-3">
-          <i className="ri-time-line p-medium"></i> {currentBlog?.time}
-        </span>
-      </Link>
-
-      <DotPagination
-        count={blogs.length}
-        index={currentBlogIndex}
-        handlePreviousClick={handlePrevBlog}
-        handleNextClick={handleNextBlog}
-      />
+      <FeaturedContentSlider>
+        {blogs.map((blog) => (
+          <div
+            key={blog._id}
+            className="px-sect pt-8 flex h-fit w-svw flex-col items-center gap-8"
+          >
+            <Link
+              to={`/inspiration/${blog._id}`}
+              target="_top"
+              className="h-[75svh] w-full overflow-hidden rounded-xl shadow-section"
+            >
+              <motion.img
+                whileHover="hoverScale"
+                transition={{ duration: 0.4 }}
+                variants={variants}
+                src={blog.image}
+                alt={blog.title}
+                className="cursor-hover h-full w-full rounded-xl cursor-pointer object-cover"
+              />
+            </Link>
+            <div className="flex flex-col items-center gap-2">
+              <span className="span-regular text-gray">{blog.category}</span>
+              <Link to={`/inspiration/${blog._id}`} target="_top">
+                <motion.h2 whileHover={{y: -3}} transition={{duration: 0.3}} className="cursor-hover-small cursor-pointer h2-md">{blog.title}</motion.h2>
+              </Link>
+            </div>
+            <p className="p-regular w-2/5 text-center">
+              {blog.content?.[0]?.sectionText?.[0]}
+            </p>
+            <span className="span-regular flex items-center gap-3">
+              <i className="ri-time-line p-medium"></i> {formatDate(blog.time)}
+            </span>
+          </div>
+        ))}
+      </FeaturedContentSlider>
     </div>
   );
 };
