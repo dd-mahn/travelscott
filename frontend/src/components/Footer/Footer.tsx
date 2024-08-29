ce07d1748966c8b820ba1ffbc5485a11b407d3bd
dd-mahn <ducmanh0801@gmail.com>
2024-08-30 02:44:11 +0700

    refactor: Update import path for StaggerLogo component in Footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 import planeIcon from "src/assets/svg/plane-icon.svg";
 import { motion } from "framer-motion";
 import { scrollToTop } from "src/utils/scrollToTop";
-import StaggerLogo from "../common/staggerLogo";
+import StaggerLogo from "../common/StaggerLogo";
 import { Link } from "react-router-dom";
 
 const sitemap = [
@@ -81,7 +81,7 @@ const Footer = () => {
             htmlFor="subscribe-email"
             className="label span-regular uppercase"
           >
-            Subscribe with email
+            Leave your email
           </label>
           <input
             type="email"
