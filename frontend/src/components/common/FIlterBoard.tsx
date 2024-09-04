831be9bace2ca826816c6206fd40e81ae93ff3b0
dd-mahn <ducmanh0801@gmail.com>
2024-09-04 23:16:45 +0700

    Add ContinentFilter for Inspiration page

--- frontend/src/components/common/FIlterBoard.tsx
+++ frontend/src/components/common/FIlterBoard.tsx
@@ -25,7 +25,11 @@ const variants = {
   visible: { opacity: 1, scale: 1, y: 0, x: 0 },
   hoverScale: {
     scale: 1.05,
-    transition: { duration: 0.4, ease: "easeInOut" },
+    transition: { duration: 0.3, ease: "easeInOut" },
+  },
+  tapScale: {
+    scale: 0.95,
+    transition: { duration: 0.3, ease: "easeInOut" },
   },
 };
 
@@ -96,7 +100,7 @@ const FullFilterBoard: React.FC<FullFilterBoardProps> = ({
     >
       <div className="flex w-full flex-row items-end gap-4">
         <div
-          className={`${inputFocus ? "border-text-light" : "border-gray border-opacity-50"} flex h-fit items-center justify-between rounded-md px-2 py-1 transition-all lg:border-[1.5px] 2xl:border-[2px]`}
+          className={`${inputFocus ? "border-text-light" : "border-gray border-opacity-50"} flex h-fit items-center justify-between rounded-md px-2 py-1 lg:border-[1.5px] 2xl:border-[2px]`}
         >
           <input
             ref={inputRef}
@@ -107,10 +111,10 @@ const FullFilterBoard: React.FC<FullFilterBoardProps> = ({
             onBlur={() => setInputFocus(false)}
           />
           <motion.button
-            whileHover={{ scale: 1.1 }}
+            whileHover="hoverScale"
             variants={variants}
-            whileTap={{ scale: 0.95 }}
-            transition={{ duration: 0.2 }}
+            whileTap="tapScale"
+            transition={{ duration: 0.3 }}
             title="search"
             className="outline-none"
             onClick={handleSearchClick}
@@ -124,22 +128,30 @@ const FullFilterBoard: React.FC<FullFilterBoardProps> = ({
         <span className="span-medium font-prima uppercase">Location</span>
         <div className="flex flex-wrap gap-2">
           {continentNames.map((continent) => (
-            <span
+            <motion.button
+              whileHover="hoverScale"
+              variants={variants}
+              transition={{ duration: 0.3 }}
+              whileTap="tapScale"
               key={continent}
-              className={`${filterContinents.includes(continent) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border border-gray px-4 transition-all hover:scale-[1.05] hover:border-text-light`}
+              className={`${filterContinents.includes(continent) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border border-gray px-4`}
               onClick={() => continentFilterClick(continent)}
             >
               {continent}
-            </span>
+            </motion.button>
           ))}
           {countryNames.map((country) => (
-            <span
+            <motion.button
+              whileHover="hoverScale"
+              variants={variants}
+              transition={{ duration: 0.3 }}
+              whileTap="tapScale"
               key={country}
-              className={`${filterCountries.includes(country) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border border-gray px-4 transition-all hover:scale-[1.05] hover:border-text-light`}
+              className={`${filterCountries.includes(country) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border border-gray px-4`}
               onClick={() => countryFilterClick(country)}
             >
               {country}
-            </span>
+            </motion.button>
           ))}
         </div>
       </div>
@@ -148,13 +160,17 @@ const FullFilterBoard: React.FC<FullFilterBoardProps> = ({
         <span className="span-medium font-prima uppercase">Tags</span>
         <div className="flex flex-wrap gap-2">
           {tags.map((tag) => (
-            <span
+            <motion.button
+              whileHover="hoverScale"
+              variants={variants}
+              transition={{ duration: 0.3 }}
+              whileTap="tapScale"
               key={tag}
-              className={`${filterTags.includes(tag) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border border-gray px-4 transition-all hover:scale-[1.05] hover:border-text-light`}
+              className={`${filterTags.includes(tag) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border border-gray px-4`}
               onClick={() => tagFilterClick(tag)}
             >
               {tag}
-            </span>
+            </motion.button>
           ))}
         </div>
       </div>
@@ -163,3 +179,75 @@ const FullFilterBoard: React.FC<FullFilterBoardProps> = ({
 };
 
 export default memo(FullFilterBoard);
+
+interface ContinentFilterProps {
+  continentFilter: string[];
+  setContinentFilter: React.Dispatch<SetStateAction<string[]>>;
+  setCurrentPage: React.Dispatch<SetStateAction<number>>;
+}
+
+const continents = [
+  "Africa",
+  "Asia",
+  "Europe",
+  "North America",
+  "South America",
+  "Oceania",
+];
+
+export const ContinentFilter: React.FC<ContinentFilterProps> = React.memo(
+  ({ continentFilter, setContinentFilter, setCurrentPage }) => {
+    const handleContinentFilter = useCallback((continent: string) => {
+      setContinentFilter((prev) =>
+        prev.includes(continent)
+          ? prev.filter((c) => c !== continent)
+          : [...prev, continent],
+      );
+      setCurrentPage(1);
+    }, [setCurrentPage]);
+
+    return (
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        variants={variants}
+        transition={{
+          duration: 0.5,
+          delay: 0.5,
+          staggerChildren: 0.2,
+          delayChildren: 0.5,
+        }}
+        className="continent-filter flex flex-row flex-nowrap justify-center lg:gap-3 2xl:gap-4 shadow-component lg:rounded-xl lg:px-8 lg:py-4 2xl:rounded-2xl 2xl:px-sect-short 2xl:py-8"
+      >
+        {continents.map((continent) => (
+          <motion.button
+            variants={variants}
+            key={continent}
+            onClick={() => handleContinentFilter(continent)}
+            whileHover="hoverScale"
+            whileTap="tapScale"
+            transition={{ duration: 0.3 }}
+            className={`continent-btn span-regular rounded-3xl border border-gray lg:px-6 lg:py-1 2xl:px-8 2xl:py-2 ${
+              continentFilter.includes(continent)
+                ? "bg-background-dark text-text-dark"
+                : "bg-transparent text-text-light"
+            }`}
+          >
+            {continent}
+          </motion.button>
+        ))}
+        <motion.button
+          variants={variants}
+          whileHover="hoverScale"
+          whileTap="tapScale"
+          transition={{ duration: 0.3 }}
+          title="filterBtn"
+          className="p-large ml-4"
+        >
+          <i className="cursor-hover-small ri-filter-2-line"></i>
+        </motion.button>
+      </motion.div>
+    );
+  },
+);
