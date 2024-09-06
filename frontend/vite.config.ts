6b0db5607670005ed1386b185d7f6960404f2c21
dd-mahn <ducmanh0801@gmail.com>
2024-09-07 02:37:46 +0700

    Add VitePWA plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -51,6 +52,12 @@ export default defineConfig({
         quality: 50,
       },
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
   ],
   resolve: {
     alias: {
