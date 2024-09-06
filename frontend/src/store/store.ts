b28f57442b28342618e7b983478272b7dbd78d68
dd-mahn <ducmanh0801@gmail.com>
2024-09-07 02:54:28 +0700

    add filterReducer for filters

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,12 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import inspirationReducer from 'src/store/slices/inspirationSlice';
 import blogReducer from 'src/store/slices/blogSlice';
 import homeReducer from 'src/store/slices/homeSlice';
-
+import filterReducer from 'src/store/slices/filterSlice';
 export const store = configureStore({
   reducer: {
     inspiration: inspirationReducer,
     blog: blogReducer,
     home: homeReducer,
+    filter: filterReducer,
   },
 });
 
