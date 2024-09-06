73acc7f5ee5b0db032b92430a64d6afc8d832629
dd-mahn <ducmanh0801@gmail.com>
2024-09-07 02:38:17 +0700

    Add Redux Provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,18 @@ import "src/App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Layout from "src/components/Layout/Layout";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 function App() {
-  return <Layout />;
+  return (
+    <Provider store={store}>
+      <ErrorBoundary>
+        <Layout />
+      </ErrorBoundary>
+    </Provider>
+  );
 }
 
 export default App;
