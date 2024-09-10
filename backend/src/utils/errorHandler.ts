a19b2c8a310727fe6175a51e12fcaaf9ca77540d
dd-mahn <ducmanh0801@gmail.com>
2024-09-10 17:15:05 +0700

    Add global error handlers

---
+++ backend/src/utils/errorHandler.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+import { sendErrorResponse } from './apiResponse';
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  sendErrorResponse(res, 'An unexpected error occurred', 500, err.message);
+};
+
