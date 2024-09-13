78be33fb62201b00fd598429b22067c7567f7f81
dd-mahn <ducmanh0801@gmail.com>
2024-09-14 01:33:31 +0700

    Reduce dependency of filter boards from their parent components

--- frontend/src/common/FIlterBoards.tsx
+++ frontend/src/common/FIlterBoards.tsx
@@ -24,11 +24,6 @@ import {
   VisibilityVariants,
 } from "src/utils/variants";
 
-interface DestinationFilterProps {
-  continentNames: string[];
-  countryNames: string[];
-}
-
 const variants = {
   hiddenY: VisibilityVariants.hiddenY,
   visible: VisibilityVariants.visible,
@@ -36,171 +31,189 @@ const variants = {
   tapScale: TapVariants.tapScale,
 };
 
-export const DestinationFilter: React.FC<DestinationFilterProps> = memo(
-  ({ continentNames, countryNames }) => {
-    const dispatch = useDispatch();
-    const selectDestinationFilterState = useCallback(
-      (state: RootState) => ({
-        continents: state.filter.destination.continents,
-        countries: state.filter.destination.countries,
-        tags: state.filter.destination.tags,
-        searchQuery: state.filter.destination.searchQuery,
-      }),
-      [],
-    );
-    const { continents, countries, tags, searchQuery } = useSelector(
-      selectDestinationFilterState,
-    );
-    const [inputFocus, setInputFocus] = useState<boolean>(false);
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [localSearchQuery, setLocalSearchQuery] = useState(searchQuery);
-    const debouncedSearchQuery = useDebounce(localSearchQuery, 500);
+export const DestinationFilter: React.FC = memo(() => {
+  const dispatch = useDispatch();
+  const selectDestinationFilterState = useCallback(
+    (state: RootState) => ({
+      continents: state.filter.destination.continents,
+      countries: state.filter.destination.countries,
+      tags: state.filter.destination.tags,
+      searchQuery: state.filter.destination.searchQuery,
+    }),
+    [],
+  );
+  const {
+    continents: filterContinents,
+    countries: filterCountries,
+    tags: filterTags,
+    searchQuery,
+  } = useSelector(selectDestinationFilterState);
+  const { countries } = useSelector((state: RootState) => state.country);
+  const { continents } = useSelector((state: RootState) => state.continent);
+  const [inputFocus, setInputFocus] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [localSearchQuery, setLocalSearchQuery] = useState(searchQuery);
+  const debouncedSearchQuery = useDebounce(localSearchQuery, 500);
 
-    const predefinedTags = useMemo(
-      () => [
-        "Wilderness",
-        "Culture&Heritage",
-        "Food&Drink",
-        "SoloJourneys",
-        "CityScape",
-        "Season&Festival",
-        "Relaxation",
-      ],
-      [],
-    );
+  const countryNames = useMemo(
+    () => countries.map((country) => country.name),
+    [countries],
+  );
 
-    const continentFilterClick = useCallback(
-      (continent: string) => {
-        dispatch(
-          setDestinationContinents(
-            continents.includes(continent)
-              ? continents.filter((item: string) => item !== continent)
-              : [...continents, continent],
-          ),
-        );
-      },
-      [continents, dispatch],
-    );
+  const continentNames = useMemo(
+    () => continents.map((continent) => continent.name),
+    [continents],
+  );
 
-    const countryFilterClick = useCallback(
-      (country: string) => {
-        dispatch(
-          setDestinationCountries(
-            countries.includes(country)
-              ? countries.filter((item: string) => item !== country)
-              : [...countries, country],
-          ),
-        );
-      },
-      [countries, dispatch],
-    );
+  const predefinedTags = useMemo(
+    () => [
+      "Wilderness",
+      "Culture&Heritage",
+      "Food&Drink",
+      "SoloJourneys",
+      "CityScape",
+      "Season&Festival",
+      "Relaxation",
+    ],
+    [],
+  );
 
-    const tagFilterClick = useCallback(
-      (tag: string) => {
-        dispatch(
-          setDestinationTags(
-            tags.includes(tag)
-              ? tags.filter((item: string) => item !== tag)
-              : [...tags, tag],
-          ),
-        );
-      },
-      [tags, dispatch],
-    );
+  const continentFilterClick = useCallback(
+    (continent: string) => {
+      dispatch(
+        setDestinationContinents(
+          filterContinents.includes(continent)
+            ? filterContinents.filter((item: string) => item !== continent)
+            : [...filterContinents, continent],
+        ),
+      );
+    },
+    [filterContinents, dispatch],
+  );
 
-    const handleSearchChange = useCallback(
-      (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLocalSearchQuery(e.target.value);
-      },
-      [],
-    );
+  const countryFilterClick = useCallback(
+    (country: string) => {
+      dispatch(
+        setDestinationCountries(
+          filterCountries.includes(country)
+            ? filterCountries.filter((item: string) => item !== country)
+            : [...filterCountries, country],
+        ),
+      );
+    },
+    [filterCountries, dispatch],
+  );
 
-    useEffect(() => {
-      dispatch(setDestinationSearchQuery(debouncedSearchQuery));
-    }, [debouncedSearchQuery, dispatch]);
+  const tagFilterClick = useCallback(
+    (tag: string) => {
+      dispatch(
+        setDestinationTags(
+          filterTags.includes(tag)
+            ? filterTags.filter((item: string) => item !== tag)
+            : [...filterTags, tag],
+        ),
+      );
+    },
+    [filterTags, dispatch],
+  );
 
-    return (
-      <motion.div
-        initial="hiddenY"
-        whileInView="visible"
-        variants={variants}
-        viewport={{ once: true }}
-        exit="hiddenY"
-        transition={{ duration: 0.4, ease: "easeInOut" }}
-        className={`filter-board absolute right-[5%] top-2/3 z-10 flex flex-col items-center gap-8 rounded-2xl bg-background-light px-4 pb-10 pt-6 shadow-section lg:w-[40svw] 2xl:w-[30svw]`}
-      >
-        <motion.div variants={variants} className="relative w-full">
+  const handleSearchChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setLocalSearchQuery(e.target.value);
+    },
+    [],
+  );
+
+  useEffect(() => {
+    dispatch(setDestinationSearchQuery(debouncedSearchQuery));
+  }, [debouncedSearchQuery, dispatch]);
+
+  return (
+    <motion.div
+      initial="hiddenY"
+      whileInView="visible"
+      variants={variants}
+      viewport={{ once: true }}
+      exit="hiddenY"
+      transition={{ duration: 0.4, ease: "easeInOut" }}
+      className={`filter-board absolute right-[5%] top-2/3 z-10 flex flex-col items-center gap-8 rounded-2xl bg-background-light px-4 pb-10 pt-6 shadow-section lg:w-[40svw] 2xl:w-[30svw]`}
+    >
+      <div className="flex w-full flex-col items-start gap-2">
+        <span className="span-medium font-prima uppercase">Search</span>
+        <div className="relative flex w-full items-center">
           <input
             ref={inputRef}
             type="text"
-            placeholder="Search..."
+            placeholder="Search destinations..."
             value={localSearchQuery}
             onChange={handleSearchChange}
             onFocus={() => setInputFocus(true)}
             onBlur={() => setInputFocus(false)}
-            className={`w-full rounded-full border border-gray bg-background-light text-text-light transition-all duration-300 focus:outline-none lg:border-[1px] lg:px-3 lg:py-1 2xl:border-[1.5px] 2xl:px-4 2xl:py-2 ${
-              inputFocus ? "border-text-light shadow-lg" : ""
-            }`}
+            className="w-full rounded-full border-[1px] border-gray bg-background-light px-4 py-2 text-text-light outline-none transition-all duration-300 focus:border-text-light focus:shadow-lg"
           />
-          <i
-            className={`ri-search-line cursor-hover-small span-regular absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer text-text-light transition-all duration-300`}
-          ></i>
-        </motion.div>
-
-        <div className="flex w-full flex-col items-start gap-2">
-          <span className="span-medium font-prima uppercase">Location</span>
-          <div className="flex flex-wrap gap-2">
-            {continentNames.map((continent) => (
-              <motion.button
-                whileHover="hoverScale"
-                variants={variants}
-                transition={{ duration: 0.3 }}
-                whileTap="tapScale"
-                key={continent}
-                className={`${continents.includes(continent) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border-gray px-4 lg:border-[1px] 2xl:border-[1.5px]`}
-                onClick={() => continentFilterClick(continent)}
-              >
-                {continent}
-              </motion.button>
-            ))}
-            {countryNames.map((country) => (
-              <motion.button
-                whileHover="hoverScale"
-                variants={variants}
-                transition={{ duration: 0.3 }}
-                whileTap="tapScale"
-                key={country}
-                className={`${countries.includes(country) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border-gray px-4 lg:border-[1px] 2xl:border-[1.5px]`}
-                onClick={() => countryFilterClick(country)}
-              >
-                {country}
-              </motion.button>
-            ))}
+          <div className="absolute right-[5%] overflow-hidden">
+            <motion.i
+              variants={variants}
+              animate={inputFocus ? "visible" : "hiddenY"}
+              className="cursor-hover-small ri-search-line cursor-pointer text-text-light"
+            ></motion.i>
           </div>
         </div>
+      </div>
 
-        <div className="flex w-full flex-col items-start gap-2">
-          <span className="span-medium font-prima uppercase">Tags</span>
-          <div className="flex flex-wrap gap-2">
-            {predefinedTags.map((tag) => (
-              <motion.button
-                whileHover="hoverScale"
-                variants={variants}
-                transition={{ duration: 0.3 }}
-                whileTap="tapScale"
-                key={tag}
-                className={`${tags.includes(tag) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border-gray px-4 lg:border-[1px] 2xl:border-[1.5px]`}
-                onClick={() => tagFilterClick(tag)}
-              >
-                {tag}
-              </motion.button>
-            ))}
-          </div>
+      <div className="flex w-full flex-col items-start gap-2">
+        <span className="span-medium font-prima uppercase">Location filter</span>
+        <div className="flex flex-wrap gap-2">
+          {continentNames.map((continent) => (
+            <motion.button
+              whileHover="hoverScale"
+              variants={variants}
+              transition={{ duration: 0.3 }}
+              whileTap="tapScale"
+              key={continent}
+              className={`${filterContinents.includes(continent) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border-gray px-4 lg:border-[1px] 2xl:border-[1.5px]`}
+              onClick={() => continentFilterClick(continent)}
+            >
+              {continent}
+            </motion.button>
+          ))}
+          {countryNames.map((country) => (
+            <motion.button
+              whileHover="hoverScale"
+              variants={variants}
+              transition={{ duration: 0.3 }}
+              whileTap="tapScale"
+              key={country}
+              className={`${filterCountries.includes(country) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border-gray px-4 lg:border-[1px] 2xl:border-[1.5px]`}
+              onClick={() => countryFilterClick(country)}
+            >
+              {country}
+            </motion.button>
+          ))}
         </div>
-      </motion.div>
-    );
-  },
-);
+      </div>
+
+      <div className="flex w-full flex-col items-start gap-2">
+        <span className="span-medium font-prima uppercase">Tag filter</span>
+        <div className="flex flex-wrap gap-2">
+          {predefinedTags.map((tag) => (
+            <motion.button
+              whileHover="hoverScale"
+              variants={variants}
+              transition={{ duration: 0.3 }}
+              whileTap="tapScale"
+              key={tag}
+              className={`${filterTags.includes(tag) ? "bg-background-dark text-text-dark" : "bg-background-light text-text-light"} span-small cursor-pointer rounded-2xl border-gray px-4 lg:border-[1px] 2xl:border-[1.5px]`}
+              onClick={() => tagFilterClick(tag)}
+            >
+              {tag}
+            </motion.button>
+          ))}
+        </div>
+      </div>
+    </motion.div>
+  );
+});
 
 export const CountryDestinationFilter: React.FC = memo(() => {
   const dispatch = useDispatch();
@@ -258,53 +271,62 @@ export const CountryDestinationFilter: React.FC = memo(() => {
   }, [debouncedSearchQuery, dispatch]);
 
   return (
-    <div className="flex w-full flex-col items-start gap-8">
-      <div className="flex w-full flex-col items-start gap-2">
-        <span className="span-medium font-prima uppercase">Search</span>
-        <div className="relative flex w-full items-center">
-          <input
-            ref={inputRef}
-            type="text"
-            placeholder="Search destinations..."
-            value={localSearchQuery}
-            onChange={handleSearchChange}
-            onFocus={() => setInputFocus(true)}
-            onBlur={() => setInputFocus(false)}
-            className="w-full rounded-full border-[1px] border-gray bg-background-light px-4 py-2 text-text-light outline-none transition-all duration-300 focus:border-text-light focus:shadow-lg"
-          />
-          <div className="overflow-hidden absolute right-[5%]">
-            <motion.i
-              variants={variants}
-              animate={inputFocus ? "visible" : "hiddenY"}
-              className="cursor-hover-small ri-search-line cursor-pointer text-text-light"
-            ></motion.i>
+    <motion.div
+      initial="hiddenY"
+      animate="visible"
+      exit="hiddenY"
+      variants={variants}
+      transition={{ duration: 0.3 }}
+      className="filter-board absolute right-[5%] top-2/3 z-10 flex flex-col items-center gap-8 rounded-xl bg-background-light px-4 pb-8 pt-4 shadow-component lg:w-[30svw] 2xl:w-[25svw]"
+    >
+      <div className="flex w-full flex-col items-start gap-8">
+        <div className="flex w-full flex-col items-start gap-2">
+          <span className="span-medium font-prima uppercase">Search</span>
+          <div className="relative flex w-full items-center">
+            <input
+              ref={inputRef}
+              type="text"
+              placeholder="Search destinations..."
+              value={localSearchQuery}
+              onChange={handleSearchChange}
+              onFocus={() => setInputFocus(true)}
+              onBlur={() => setInputFocus(false)}
+              className="w-full rounded-full border-[1px] border-gray bg-background-light px-4 py-2 text-text-light outline-none transition-all duration-300 focus:border-text-light focus:shadow-lg"
+            />
+            <div className="absolute right-[5%] overflow-hidden">
+              <motion.i
+                variants={variants}
+                animate={inputFocus ? "visible" : "hiddenY"}
+                className="cursor-hover-small ri-search-line cursor-pointer text-text-light"
+              ></motion.i>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex w-full flex-col items-start gap-2">
+          <span className="span-medium font-prima uppercase">Tag filter</span>
+          <div className="flex flex-wrap gap-2">
+            {predefinedTags.map((tag) => (
+              <motion.button
+                whileHover="hoverScale"
+                variants={variants}
+                transition={{ duration: 0.3 }}
+                whileTap="tapScale"
+                key={tag}
+                className={`${
+                  selectTags.includes(tag)
+                    ? "bg-background-dark text-text-dark"
+                    : "bg-background-light text-text-light"
+                } span-small cursor-pointer rounded-2xl border-gray px-4 lg:border-[1px] 2xl:border-[1.5px]`}
+                onClick={() => handleTagFilter(tag)}
+              >
+                {tag}
+              </motion.button>
+            ))}
           </div>
         </div>
       </div>
-
-      <div className="flex w-full flex-col items-start gap-2">
-        <span className="span-medium font-prima uppercase">Tags</span>
-        <div className="flex flex-wrap gap-2">
-          {predefinedTags.map((tag) => (
-            <motion.button
-              whileHover="hoverScale"
-              variants={variants}
-              transition={{ duration: 0.3 }}
-              whileTap="tapScale"
-              key={tag}
-              className={`${
-                selectTags.includes(tag)
-                  ? "bg-background-dark text-text-dark"
-                  : "bg-background-light text-text-light"
-              } span-small cursor-pointer rounded-2xl border-gray px-4 lg:border-[1px] 2xl:border-[1.5px]`}
-              onClick={() => handleTagFilter(tag)}
-            >
-              {tag}
-            </motion.button>
-          ))}
-        </div>
-      </div>
-    </div>
+    </motion.div>
   );
 });
 
