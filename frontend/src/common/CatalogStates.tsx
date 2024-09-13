c371a1acd8a8c6b6f07d36baa915786420facc00
dd-mahn <ducmanh0801@gmail.com>
2024-09-14 02:11:03 +0700

    Fix prop name error

--- frontend/src/common/CatalogStates.tsx
+++ frontend/src/common/CatalogStates.tsx
@@ -2,47 +2,51 @@ import React from "react";
 import { motion } from "framer-motion";
 import { VisibilityVariants } from "src/utils/variants";
 
-export const LoadingState: React.FC<{ key: string }> = React.memo(({ key }) => {
-  return (
-    <motion.div
-      key={key}
-      initial="hiddenY"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={VisibilityVariants}
-      exit="hiddenShort"
-      transition={{ duration: 0.5, ease: "easeInOut" }}
-      className="grid h-[50svh] w-full place-items-center py-sect-short"
-    >
-      <h3 className="h3-md">Loading...</h3>
-    </motion.div>
-  );
-});
-
-export const ErrorState: React.FC<{ key: string }> = React.memo(({ key }) => {
-  return (
-    <motion.div
-      key={key}
-      initial="hiddenY"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={VisibilityVariants}
-      exit="hiddenShort"
-      transition={{ duration: 0.5, ease: "easeInOut" }}
-      className="grid h-[50svh] w-full place-items-center py-sect-short"
-    >
-      <h3 className="h3-md">
-        Error... Please reload the page or try again later.
-      </h3>
-    </motion.div>
-  );
-});
-
-export const NotFoundState: React.FC<{ key: string }> = React.memo(
-  ({ key }) => {
+export const LoadingState: React.FC<{ keyName: string }> = React.memo(
+  ({ keyName }) => {
     return (
       <motion.div
-        key={key}
+        key={keyName}
+        initial="hiddenY"
+        whileInView="visible"
+        viewport={{ once: true }}
+        variants={VisibilityVariants}
+        exit="hiddenShort"
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className="grid h-[50svh] w-full place-items-center py-sect-short"
+      >
+        <h3 className="h3-md">Loading...</h3>
+      </motion.div>
+    );
+  },
+);
+
+export const ErrorState: React.FC<{ keyName: string }> = React.memo(
+  ({ keyName }) => {
+    return (
+      <motion.div
+        key={keyName}
+        initial="hiddenY"
+        whileInView="visible"
+        viewport={{ once: true }}
+        variants={VisibilityVariants}
+        exit="hiddenShort"
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className="grid h-[50svh] w-full place-items-center py-sect-short"
+      >
+        <h3 className="h3-md">
+          Error... Please reload the page or try again later.
+        </h3>
+      </motion.div>
+    );
+  },
+);
+
+export const NotFoundState: React.FC<{ keyName: string }> = React.memo(
+  ({ keyName }) => {
+    return (
+      <motion.div
+        key={keyName}
         initial="hiddenY"
         whileInView="visible"
         viewport={{ once: true }}
