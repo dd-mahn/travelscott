682339efe5847342743eb574e83de97d4c8e94aa
dd-mahn <ducmanh0801@gmail.com>
2024-09-14 02:49:14 +0700

    Add optimizeImages

--- frontend/src/common/FeaturedBlogs.tsx
+++ frontend/src/common/FeaturedBlogs.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
@@ -6,6 +6,8 @@ import Blog from "src/types/Blog";
 import FeaturedContentSlider from "./FeaturedContentSlider";
 import { formatDate } from "src/utils/formatDate";
 import { HoverVariants, TapVariants } from "src/utils/variants";
+import { getImageSize, useViewportWidth } from "src/utils/imageUtils";
+import { optimizeImage } from "src/utils/optimizeImage";
 
 type featuredBlogsProps = {
   blogs: Blog[];
@@ -17,32 +19,63 @@ const variants = {
 };
 
 const FeaturedBlogs: React.FC<featuredBlogsProps> = ({ blogs }) => {
+  const viewportWidth = useViewportWidth();
+  const imageSize = getImageSize(viewportWidth);
+
+  const optimizedBlogs = useMemo(() => {
+    return blogs.map((blog) => ({
+      ...blog,
+      image: optimizeImage(blog.image, {
+        width: imageSize,
+        quality: 100,
+        format: "auto",
+      }),
+    }));
+  }, [blogs, imageSize]);
+
   return (
-    <div className="flex flex-col items-center gap-8 pb-sect-default w-screen">
+    <div className="flex w-screen flex-col items-center gap-8 pb-sect-default">
       <FeaturedContentSlider>
-        {blogs.map((blog) => (
+        {optimizedBlogs.map((blog) => (
           <div
             key={blog._id}
-            className="px-sect pt-8 flex h-fit w-svw flex-col items-center gap-8"
+            className="px-sect flex h-fit w-svw flex-col items-center gap-8 pt-8"
           >
             <Link
               to={`/inspiration/${blog._id}`}
               target="_top"
-              className="h-[75svh] w-full overflow-hidden rounded-xl shadow-section bg-gradient-to-t from-background-dark to-transparent"
+              className="h-[75svh] w-full overflow-hidden rounded-xl bg-gradient-to-t from-blue-gray-900 to-gray shadow-section"
             >
               <motion.img
                 whileHover="hoverScale"
                 transition={{ duration: 0.4 }}
                 variants={variants}
-                src={blog.image}
+                src={
+                  blog.image
+                    ? typeof blog.image === "string"
+                      ? blog.image
+                      : blog.image.src || ""
+                    : ""
+                }
+                srcSet={
+                  blog.image && typeof blog.image !== "string"
+                    ? blog.image.srcSet || ""
+                    : ""
+                }
                 alt={blog.title}
-                className="cursor-hover h-full w-full rounded-xl cursor-pointer object-cover"
+                className="cursor-hover h-full w-full cursor-pointer rounded-xl object-cover"
               />
             </Link>
             <div className="flex flex-col items-center gap-2">
               <span className="span-regular text-gray">{blog.category}</span>
               <Link to={`/inspiration/${blog._id}`} target="_top">
-                <motion.h2 whileHover={{y: -3}} transition={{duration: 0.3}} className="cursor-hover-small cursor-pointer h2-md">{blog.title}</motion.h2>
+                <motion.h2
+                  whileHover={{ y: -3 }}
+                  transition={{ duration: 0.3 }}
+                  className="cursor-hover-small h2-md cursor-pointer"
+                >
+                  {blog.title}
+                </motion.h2>
               </Link>
             </div>
             <p className="p-regular w-2/5 text-center">
