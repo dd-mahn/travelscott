96e7975ce63c30cd69bc32ed25cb0edf1f1562ad
dd-mahn <ducmanh0801@gmail.com>
2024-09-14 01:37:55 +0700

    Add continents util to store continent base datas

---
+++ frontend/src/utils/continents.ts
@@ -0,0 +1,46 @@
+// Asset imports
+import asiaMap from "src/assets/images/ui/maps/Asia.webp";
+import europeMap from "src/assets/images/ui/maps/Europe.webp";
+import africaMap from "src/assets/images/ui/maps/Africa.webp";
+import northAmericaMap from "src/assets/images/ui/maps/NorthAmerica.webp";
+import southAmericaMap from "src/assets/images/ui/maps/SouthAmerica.webp";
+import oceaniaMap from "src/assets/images/ui/maps/Oceania.webp";
+
+export const continents = [
+  {
+    name: "Asia",
+    image: asiaMap,
+    countries: [],
+    count: 0,
+  },
+  {
+    name: "Africa",
+    image: africaMap,
+    countries: [],
+    count: 0,
+  },
+  {
+    name: "Europe",
+    image: europeMap,
+    countries: [],
+    count: 0,
+  },
+  {
+    name: "North America",
+    image: northAmericaMap,
+    countries: [],
+    count: 0,
+  },
+  {
+    name: "South America",
+    image: southAmericaMap,
+    countries: [],
+    count: 0,
+  },
+  {
+    name: "Oceania",
+    image: oceaniaMap,
+    countries: [],
+    count: 0,
+  },
+];
